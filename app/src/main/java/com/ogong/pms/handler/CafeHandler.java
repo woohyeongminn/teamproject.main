03d153ecdc6e82c3157b1c344abfa21915c53d72
ssongdahye <songdh121@gmail.com>
2021-08-26 16:05:22 +0900

    ,

--- app/src/main/java/com/ogong/pms/handler/CafeHandler.java
+++ app/src/main/java/com/ogong/pms/handler/CafeHandler.java
@@ -2,15 +2,18 @@ package com.ogong.pms.handler;
 
 import java.util.List;
 import com.ogong.pms.domain.Cafe;
+import com.ogong.pms.domain.Member;
 import com.ogong.util.Prompt;
 
 public class CafeHandler {
 
   List<Cafe> cafeList;
+  List<Member> memberHandler;
   int no;
 
-  public CafeHandler (List<Cafe> cafeList) {
+  public CafeHandler (List<Cafe> cafeList, List<Member> memberHandler) {
     this.cafeList = cafeList;
+    this.memberHandler = memberHandler;
   }
 
   public void add () {
@@ -24,8 +27,15 @@ public class CafeHandler {
     cafe.setLocation(Prompt.inputString("주소? "));
     cafe.setPhone(Prompt.inputString("전화번호? "));
     cafe.setHrs(Prompt.inputString("운영시간? "));
-    cafe.setPersonalSeat(Prompt.inputString("개인좌석유무? "));
+    cafe.setPersonalSeat(Prompt.inputString("개인좌석유무?(y/n) "));
+    cafe.setRoom(Prompt.inputString("미팅룸유무?(y/n) "));
+    cafe.setBookable(Prompt.inputInt("예약가능인원? "));
+    cafe.setUsingNotebooks(Prompt.inputString("노트북대여?(y/n) "));
+    cafe.setUsingCopyMachine(Prompt.inputString("복사기사용?(y/n) "));
+    cafe.setUsingWifi(Prompt.inputString("와이파이유무?(y/n) "));
+    cafe.setDrinksProvided(Prompt.inputString("음료제공유무?(y/n) "));
 
+    cafeList.add(cafe);
   }
 
 }
