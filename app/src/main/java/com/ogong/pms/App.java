00fbca329c4392fb06f4a156f3d3b81b9b124b82
woohyeongminn <gudals.woo@gmail.com>
2021-08-26 19:46:00 +0900

    test

--- app/src/main/java/com/ogong/pms/App.java
+++ app/src/main/java/com/ogong/pms/App.java
@@ -6,10 +6,12 @@ import java.util.List;
 import com.ogong.menu.Menu;
 import com.ogong.menu.MenuGroup;
 import com.ogong.pms.domain.Cafe;
+import com.ogong.pms.domain.Login;
 import com.ogong.pms.domain.Member;
 import com.ogong.pms.domain.NoticeBoard;
 import com.ogong.pms.domain.Study;
 import com.ogong.pms.handler.CafeHandler;
+import com.ogong.pms.handler.LoginHandler;
 import com.ogong.pms.handler.MemberHandler;
 import com.ogong.pms.handler.NewStudyHandler;
 import com.ogong.pms.handler.NoticeBoardHandler;
@@ -18,13 +20,18 @@ import com.ogong.util.Prompt;
 public class App {
   List<Study> studyList = new LinkedList<>();
   NewStudyHandler newStudyHandler = new NewStudyHandler(studyList);
+
   List<Member> memberList = new LinkedList<>();
   MemberHandler memberHandler = new MemberHandler(memberList);
+
   List<NoticeBoard> noticeBoardList = new ArrayList<>();
   NoticeBoardHandler noticeboardHandler = new NoticeBoardHandler(noticeBoardList);
 
   List<Cafe> cafeList = new ArrayList<>();
   CafeHandler cafeHandler = new CafeHandler(cafeList, memberList);
+
+  List<Login> loginList = new ArrayList<>();
+  LoginHandler loginHandler = new LoginHandler(loginList, memberHandler);
 
   public static void main(String[] args) {
     App app = new App(); 
@@ -35,38 +42,56 @@ public class App {
     Prompt.close();
   }
   Menu createMenu() {
+
     //--------------------------------------------------------------------
+
+    ////////////////////////////////////////////////////////
     MenuGroup mainMenuGroup = new MenuGroup("메인");
     mainMenuGroup.setPrevMenuTitle("종료");
+    ////////////////////////////////////////////////////////
+
+    MenuGroup loginMenu = new MenuGroup("[로그인]");
+    mainMenuGroup.add(loginMenu);
+
+    loginMenu.add(new Menu("로그인") {
+      @Override
+      public void execute() {
+        loginHandler.addLoginPage(); 
+      }});
+
+    //--------------------------------------------------------------------
     MenuGroup studyMenu = new MenuGroup("[모든 스터디]");
     mainMenuGroup.add(studyMenu);
-    studyMenu.add(new Menu("등록") {
+
+    studyMenu.add(new Menu("내 스터디 등록하기") {
       @Override
       public void execute() {
         newStudyHandler.add(); 
       }});
-    studyMenu.add(new Menu("목록") {
+    studyMenu.add(new Menu("스터디 목록보기") {
       @Override
       public void execute() {
         newStudyHandler.list(); 
       }});
-    studyMenu.add(new Menu("상세보기") {
+    studyMenu.add(new Menu("스터디 상세보기") {
       @Override
       public void execute() {
         newStudyHandler.detail(); 
       }});
-    studyMenu.add(new Menu("변경") {
-      @Override
-      public void execute() {
-        newStudyHandler.update(); 
-      }});
-    //    studyMenu.add(new Menu("삭제") {
+    //    studyMenu.add(new Menu("내 스터디 변경하기") {
+    //      @Override
+    //      public void execute() {
+    //        newStudyHandler.update(); 
+    //      }});
+    //    studyMenu.add(new Menu("내 스터디 삭제하기") {
     //      public void execute() {
     //        newStudyHandler.delete(); 
     //      }});
+
     //--------------------------------------------------------------------
     MenuGroup memberMenu = new MenuGroup("[개인회원]");
     mainMenuGroup.add(memberMenu);
+
     memberMenu.add(new Menu("등록하기") {
       @Override
       public void execute() {
@@ -92,9 +117,11 @@ public class App {
       public void execute() {
         memberHandler.delete(); 
       }});
+
     //--------------------------------------------------------------------
     MenuGroup noticeMenu = new MenuGroup("[공지사항 게시판]");
     mainMenuGroup.add(noticeMenu);
+
     noticeMenu.add(new Menu("공지사항") {
       public void execute() {
         noticeboardHandler.add(); 
--- app/src/main/java/com/ogong/pms/handler/LoginHandler.java
+++ app/src/main/java/com/ogong/pms/handler/LoginHandler.java
@@ -1,7 +1,31 @@
 package com.ogong.pms.handler;
 
+import java.util.List;
+import com.ogong.pms.domain.Login;
+
 public class LoginHandler {
 
+  List<Login> loginList;
+  MemberHandler memberHandler;
+
+  public LoginHandler(List<Login> loginList, MemberHandler memberHandler) {
+    this.loginList = loginList;
+    this.memberHandler = memberHandler;
+  }
+
+  public void addLoginPage() {
+
+
+  }
+
+  //    Login login = new Login();
+  //    login.setUserEmail(memberHandler.promptMember("이메일: "));
+  //
+  //    if (login.getUserEmail() == null) {
+  //      return;
+  //    } else if (login.getUserEmail()) {
+  //      System.out.println("로그인되었습니다");
+  //    }
 
 
 }
