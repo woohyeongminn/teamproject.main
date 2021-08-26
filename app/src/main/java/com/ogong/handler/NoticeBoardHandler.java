069261420b7f6ea86524abbe92e740f637ecaac2
jo-sol <zoixssm@gmail.com>
2021-08-26 14:11:50 +0900

    0826-sol

---
+++ app/src/main/java/com/ogong/handler/NoticeBoardHandler.java
@@ -0,0 +1,125 @@
+package com.ogong.handler;
+
+import java.util.List;
+import com.ogong.pms.domain.NoticeBoard;
+import com.ogong.util.Prompt;
+
+public class NoticeBoardHandler {
+
+  List<NoticeBoard> boardList;
+
+  public NoticeBoardHandler(List<NoticeBoard> boardList) {
+    this.boardList = boardList;
+  }
+
+  //  public void add() {
+  //    System.out.println("[공지사항]");
+  //
+  //    NoticeBoard notice = new NoticeBoard();
+  //
+  //    notice.setNo(Prompt.inputInt("번호? "));
+  //    notice.setTitle(Prompt.inputString("제목? "));
+  //    notice.setContent(Prompt.inputString("내용? "));
+  //    notice.setWriter(Prompt.inputString("작성자? "));
+  //    notice.setRegisteredDate(new Date(System.currentTimeMillis()));
+  //
+  //    boardList.add(notice);
+  //  }
+
+  public void list() {
+    System.out.println("[공지사항 목록]");
+
+    NoticeBoard[] notices = new NoticeBoard[boardList.size()];
+
+    boardList.toArray(notices);
+
+    for (NoticeBoard notice : notices) {
+      System.out.printf("%d, %s, %s, %d\n", 
+          notice.getNo(), 
+          notice.getTitle(), 
+          notice.getWriter(),
+          notice.getRegisteredDate());
+    }
+  }
+
+  public void detail() {
+    System.out.println("[공지사항 상세보기]");
+    int no = Prompt.inputInt("번호? ");
+
+    NoticeBoard notice = findByNo(no);
+
+    if (notice == null) {
+      System.out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
+
+    System.out.printf("제목: %s\n", notice.getTitle());
+    System.out.printf("내용: %s\n", notice.getContent());
+    System.out.printf("작성자: %s\n", notice.getWriter());
+    System.out.printf("등록일: %d\n", notice.getRegisteredDate());
+  }
+
+  //  public void update() {
+  //    System.out.println("[공지사항 변경]");
+  //    int no = Prompt.inputInt("번호? ");
+  //
+  //    NoticeBoard notice = findByNo(no);
+  //
+  //    if (notice == null) {
+  //      System.out.println("해당 번호의 게시글이 없습니다.");
+  //      return;
+  //    }
+  //
+  //    String title = Prompt.inputString(String.format("제목(%s)? ", notice.getTitle()));
+  //    String content = Prompt.inputString(String.format("내용(%s)? ", notice.getContent()));
+  //
+  //    String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
+  //    if (input.equalsIgnoreCase("n") || input.length() == 0) {
+  //      System.out.println("게시글 변경을 취소하였습니다.");
+  //      return;
+  //    }
+  //
+  //    notice.setTitle(title);
+  //    notice.setContent(content);
+  //    System.out.println("게시글을 변경하였습니다.");
+  //  }
+  //
+  //  public void delete() {
+  //    System.out.println("[공지사항 삭제]");
+  //    int no = Prompt.inputInt("번호? ");
+  //
+  //    NoticeBoard notice = findByNo(no);
+  //
+  //    if (notice == null) {
+  //      System.out.println("해당 번호의 게시글이 없습니다.");
+  //      return;
+  //    }
+  //
+  //    String input = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ");
+  //    if (input.equalsIgnoreCase("n") || input.length() == 0) {
+  //      System.out.println("게시글 삭제를 취소하였습니다.");
+  //      return;
+  //    }
+  //
+  //    boardList.remove(notice);
+  //
+  //    System.out.println("게시글을 삭제하였습니다.");
+  //  }
+
+  private NoticeBoard findByNo(int no) {
+    NoticeBoard[] arr = boardList.toArray(new NoticeBoard[0]);
+    for (NoticeBoard notice : arr) {
+      if (notice.getNo() == no) {
+        return notice;
+      }
+    }
+    return null;
+  }
+}
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/ogong/pms/domain/NoticeBoard.java
@@ -0,0 +1,42 @@
+package com.ogong.pms.domain;
+
+import java.sql.Date;
+
+public class NoticeBoard {
+  private int no;
+  private String title;
+  private String content;
+  private String writer;
+  private Date registeredDate;
+
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getTitle() {
+    return title;
+  }
+  public void setTitle(String title) {
+    this.title = title;
+  }
+  public String getContent() {
+    return content;
+  }
+  public void setContent(String content) {
+    this.content = content;
+  }
+  public String getWriter() {
+    return writer;
+  }
+  public void setWriter(String writer) {
+    this.writer = writer;
+  }
+  public Date getRegisteredDate() {
+    return registeredDate;
+  }
+  public void setRegisteredDate(Date registeredDate) {
+    this.registeredDate = registeredDate;
+  }
+}
