e0c387646a73f136cfec2145f6f9787ad051f3f2
Kimeunchaee <eun00218@naver.com>
2021-08-28 11:44:08 +0900

    0828 commit push 자유게시판도메인,핸들러 한던거 추가함

--- app/src/main/java/com/ogong/pms/App.java
+++ app/src/main/java/com/ogong/pms/App.java
@@ -23,14 +23,6 @@ import com.ogong.pms.handler.NewStudyHandler;
 import com.ogong.pms.handler.NoticeBoardHandler;
 import com.ogong.util.Prompt;
 
-// 123
-// test woo
-// 자 여러분!
-// 내용을 바꾸겠습니다
-// 이런이런
-// 와우 WoW~
-
-
 public class App {
   List<Study> studyList = new LinkedList<>();
   NewStudyHandler newStudyHandler = new NewStudyHandler(studyList);
---
+++ app/src/main/java/com/ogong/pms/domain/freeBoard.java
@@ -0,0 +1,15 @@
+package com.ogong.pms.domain;
+
+import java.sql.Date;
+
+public class freeBoard {
+
+  private int freeBoardNo;
+  private String freeBoardTitle;
+  private String freeBoardWriter;
+  private String freeBoardViewcount;
+
+  private Date freeBoardRegisteredDate;
+
+
+}
