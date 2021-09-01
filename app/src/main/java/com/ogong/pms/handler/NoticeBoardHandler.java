dab9a532346af537630ad9e30ab69444143ae557
jo-sol <zoixssm@gmail.com>
2021-09-01 17:14:31 +0900

    0901 commit

--- app/src/main/java/com/ogong/pms/handler/NoticeBoardHandler.java
+++ app/src/main/java/com/ogong/pms/handler/NoticeBoardHandler.java
@@ -17,20 +17,6 @@ public class NoticeBoardHandler {
     this.managerHandler = managerHandler;
   }
 
-  //  public void add() {
-  //    System.out.println("[공지사항]");
-  //
-  //    NoticeBoard notice = new NoticeBoard();
-  //
-  //    notice.setNotiNo(Prompt.inputInt("번호? "));
-  //    notice.setNotiTitle(Prompt.inputString("제목? "));
-  //    notice.setNotiContent(Prompt.inputString("내용? "));
-  //    notice.setNotiWriter(Prompt.inputString("작성자? "));
-  //    notice.setNotiRegisteredDate(new Date(System.currentTimeMillis()));
-  //
-  //    noticeBoardList.add(notice);
-  //  }
-
   public void list() {
     System.out.println("[공지사항 목록]");
 
@@ -62,64 +48,17 @@ public class NoticeBoardHandler {
     System.out.printf("등록일: %s\n", manager.getHostRegisteredDate());
   }
 
-  //    public void update() {
-  //      System.out.println("[공지사항 변경]");
-  //      int notiNo = Prompt.inputInt("번호? ");
-  //  
-  //      NoticeBoard notice = findByNo(notiNo);
-  //  
-  //      if (notice == null) {
-  //        System.out.println("해당 번호의 게시글이 없습니다.");
-  //        return;
-  //      }
-  //  
-  //      String notiTitle = Prompt.inputString(String.format("제목(%s)? ", notice.getNotiTitle()));
-  //      String notiContent = Prompt.inputString(String.format("내용(%s)? ", notice.getNotiContent()));
-  //  
-  //      String input = Prompt.inputString("정말 변경하시겠습니까?(y/N) ");
-  //      if (input.equalsIgnoreCase("n") || input.length() == 0) {
-  //        System.out.println("게시글 변경을 취소하였습니다.");
-  //        return;
-  //      }
-  //  
-  //      notice.setNotiTitle(notiTitle);
-  //      notice.setNotiContent(notiContent);
-  //      System.out.println("게시글을 변경하였습니다.");
-  //    }
-  //  
-  //    public void delete() {
-  //      System.out.println("[공지사항 삭제]");
-  //      int notiNo = Prompt.inputInt("번호? ");
-  //  
-  //      NoticeBoard notice = findByNo(notiNo);
-  //  
-  //      if (notice == null) {
-  //        System.out.println("해당 번호의 게시글이 없습니다.");
-  //        return;
-  //      }
-  //  
-  //      String input = Prompt.inputString("정말 삭제하시겠습니까?(y/N) ");
-  //      if (input.equalsIgnoreCase("n") || input.length() == 0) {
-  //        System.out.println("게시글 삭제를 취소하였습니다.");
-  //        return;
-  //      }
-  //  
-  //      noticeBoardList.remove(notice);
-  //  
-  //      System.out.println("게시글을 삭제하였습니다.");
-  //    }
+  public void findByHostNo() {
+    System.out.println("[공지사항]");
+    int hostNo = Prompt.inputInt("번호: ");
 
-  //  public void findByHostNo() {
-  //    System.out.println("[공지사항]");
-  //    int hostNo = Prompt.inputInt("번호: ");
-  //
-  //    Manager manager = managerHandler.findByHostNo(hostNo);
-  //
-  //    if (manager == null) {
-  //      System.out.println("해당 번호의 공지글이 없습니다.");
-  //      return;
-  //    }
-  //  }
+    Manager manager = managerHandler.findByHostNo(hostNo);
+
+    if (manager == null) {
+      System.out.println("해당 번호의 공지글이 없습니다.");
+      return;
+    }
+  }
 }
 
 
