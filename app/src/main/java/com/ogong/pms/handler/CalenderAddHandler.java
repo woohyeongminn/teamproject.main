93289c2e7d83b66a805883c5182dcdcb15c6694e
woohyeongminn <gudals.woo@gmail.com>
2021-09-08 18:03:48 +0900

    0908 push 캘린더 (업데이트 삭제) 수정

--- app/src/main/java/com/ogong/pms/handler/CalenderAddHandler.java
+++ app/src/main/java/com/ogong/pms/handler/CalenderAddHandler.java
@@ -2,17 +2,13 @@ package com.ogong.pms.handler;
 
 import java.util.List;
 import com.ogong.pms.domain.Calender;
-import com.ogong.pms.domain.Study;
 import com.ogong.util.Prompt;
 
 
 public class CalenderAddHandler extends AbstractCalenderHandler {
 
-  List<Study> studyList;
-
-  public CalenderAddHandler( List<Calender> calenderList, List<Study> studyList) {
+  public CalenderAddHandler( List<Calender> calenderList) {
     super(calenderList);
-    this.studyList = studyList;
   }
 
   @SuppressWarnings("deprecation")
@@ -26,53 +22,30 @@ public class CalenderAddHandler extends AbstractCalenderHandler {
     System.out.println();
     System.out.println("시작 날짜를 입력해 주세요.");
 
-    int month = Prompt.inputInt("월(1~12) : ");
-    if (month > 12) {
-      System.out.println("등록할 수 없는 '월'입니다.");
-      System.out.println("다시 등록해주세요.");
-      return;
-
+    int month; 
+    while (true) {
+      month = Prompt.inputInt("월(1~12) : ");
+      if (month > 12) {
+        System.out.println("\n등록할 수 없는 '월'입니다.");
+        System.out.println("다시 등록해주세요.");
+        continue;
+      }
+      break;
     }
     calender.setMonth(month);
 
     int day;
     while (true) {
       day = Prompt.inputInt("일(1~31) : ");
-      if (month == 2) {
-        if (day > 28 || day < 1) {
-          System.out.println("등록할 수 없는 '일'입니다.");
-          System.out.println("다시 등록해주세요.");
-          continue;
-        }
+      if (((month == 2) && (day > 28 || day < 1)) ||
+          ((((month < 8 && (month % 2) == 1) && (day > 31 || day < 1)) ||
+              ((month < 8 && (month % 2) == 0) && (day > 30 || day < 1))) ||
+              (((month >= 8 && (month % 2) == 1) && (day > 30 || day < 1)) ||
+                  ((month >= 8 && (month % 2) == 0) && (day > 31 || day < 1))))) {
+        System.out.println("등록할 수 없는 '일'입니다.");
+        System.out.println("다시 등록해주세요.");
+        continue;
       } 
-      else if (month < 8 && (month % 2) == 1) {
-        if (day > 31 || day < 1) {
-          System.out.println("등록할 수 없는 '일'입니다.");
-          System.out.println("다시 등록해주세요.");
-          continue;
-        }
-      }
-      else if (month < 8 && (month % 2) == 0) {
-        if (day > 30 || day < 1) {
-          System.out.println("등록할 수 없는 '일'입니다.");
-          System.out.println("다시 등록해주세요.");
-          continue;
-        }
-      }
-      else if (month >= 8 && (month % 2) == 1) {
-        if (day > 30 || day < 1) {
-          System.out.println("등록할 수 없는 '일'입니다.");
-          System.out.println("다시 등록해주세요.");
-          continue;
-        }
-      }
-      else if (month >= 8 && (month % 2) == 0) {
-        if (day > 31 || day < 1) {
-          System.out.println("등록할 수 없는 '일'입니다.");
-          System.out.println("다시 등록해주세요.");
-          continue;
-        }
-      }
       break;
     }
     calender.setDay(day);
@@ -93,18 +66,18 @@ public class CalenderAddHandler extends AbstractCalenderHandler {
     }
 
     calender.setCalenderContent(Prompt.inputString("내용 : "));
+
     while (true) {
       calender.setEndDay(Prompt.inputDate("종료일 : "));
-      if (calender.getEndDay().getMonth() + 1 <= month) {
-        if (calender.getEndDay().getDate() < day) {
-          System.out.println("종료일을 다시 입력해주세요.");
-          continue;
-        }
+      if ((calender.getEndDay().getMonth() + 1 <= month) && 
+          (calender.getEndDay().getDate() < day)) {
+        System.out.println("\n종료일을 다시 입력해주세요.");
+        continue;
       }
       break;
     }
 
-    String input = Prompt.inputString("등록하시겠습니까? (네 / 아니오)");
+    String input = Prompt.inputString("\n등록하시겠습니까? (네 / 아니오)");
     if (!input.equals("네")) {
       System.out.println("등록을 취소하였습니다.");
       return;
--- app/src/main/java/com/ogong/pms/handler/CalenderUpdateHandler.java
+++ app/src/main/java/com/ogong/pms/handler/CalenderUpdateHandler.java
@@ -18,19 +18,72 @@ public class CalenderUpdateHandler extends AbstractCalenderHandler {
     System.out.println();
     System.out.println("▶ 일정 수정");
 
-    int inputDay = Prompt.inputInt("날짜 : ");
-    Calender calender = findByDay(inputDay);
-
-    if (calender == null) {
-      System.out.println("해당 날짜의 일정이 없습니다.");
-      return;
+    int inputDay;
+    Calender calender;
+    while (true) {
+      inputDay = Prompt.inputInt("일 : ");
+      calender = findByDay(inputDay);
+      if (calender == null) {
+        System.out.println("해당 날짜의 일정이 없습니다.");
+        System.out.println("다시 입력해주세요.");
+        continue;
+      }
+      break;
+    }
+    int cMonth;
+    while (true) {
+      cMonth = Prompt.inputInt("월(" + calender.getMonth()  + ")? ");
+      if (cMonth > 12 || cMonth < 1) {
+        System.out.println("\n등록할 수 없는 '월'입니다.");
+        System.out.println("다시 등록해주세요.");
+        continue;
+      }
+      break;
     }
 
-    int cMonth = Prompt.inputInt("월(" + calender.getMonth()  + ")? ");
-    int cDay = Prompt.inputInt("일(" + calender.getDay() + ")? ");
-    String cDayOfTheWeek = Prompt.inputString("요일(" + calender.getDayOftheWeek() + ")? ");
+    int cDay;
+    while (true) {
+      cDay = Prompt.inputInt("일(" + calender.getDay() + ")? ");
+      if (((cMonth == 2) && (cDay > 28 || cDay < 1)) ||
+          ((((cMonth < 8 && (cMonth % 2) == 1) && (cDay > 31 || cDay < 1)) ||
+              ((cMonth < 8 && (cMonth % 2) == 0) && (cDay > 30 || cDay < 1))) ||
+              (((cMonth >= 8 && (cMonth % 2) == 1) && (cDay > 30 || cDay < 1)) ||
+                  ((cMonth >= 8 && (cMonth % 2) == 0) && (cDay > 31 || cDay < 1))))) {
+        System.out.println("등록할 수 없는 '일'입니다.");
+        System.out.println("다시 등록해주세요.");
+        continue;
+      } 
+      break;
+    }
+
+
+    String cDayOfTheWeek;
+    while (true) {
+      cDayOfTheWeek = Prompt.inputString("요일(" + calender.getDayOftheWeek() + ")? ");
+      if (cDayOfTheWeek.equals("월") || cDayOfTheWeek.equals("화") ||cDayOfTheWeek.equals("수") ||
+          cDayOfTheWeek.equals("목") || cDayOfTheWeek.equals("금") || cDayOfTheWeek.equals("토") ||
+          cDayOfTheWeek.equals("일")) {
+        calender.setDayOftheWeek(cDayOfTheWeek);
+        break;
+      }
+      else {
+        System.out.println("등록할 수 없는 '요일'입니다.");
+      }
+    }   
+
     String cContent = Prompt.inputString("내용(" + calender.getCalenderContent() + ")? ");
-    Date cEndDay = Prompt.inputDate("종료일(" + calender.getEndDay() + ")? ");
+
+    Date cEndDay; 
+    while (true) {
+      cEndDay =  Prompt.inputDate("종료일(" + calender.getEndDay() + ")? ");
+      calender.setEndDay(Prompt.inputDate("종료일 : "));
+      if ((calender.getEndDay().getMonth() + 1 <= cMonth) && 
+          (calender.getEndDay().getDate() < cDay)) {
+        System.out.println("\n종료일을 다시 입력해주세요.");
+        continue;
+      }
+      break;
+    }
 
     String input = Prompt.inputString("정말 변경하시겠습니까? (네 / 아니오) ");
     if (input.equalsIgnoreCase("아니오") || input.length() == 0) {
