9f42a019f3a49e816b481bae33c291a652022f0d
Kimeunchaee <eun00218@naver.com>
2021-09-14 19:47:23 +0900

    0914 commit 인원수 (참여중/전체) 로 표시되도록 구현 완료

--- app/src/main/java/com/ogong/pms/handler/AbstractStudyHandler.java
+++ app/src/main/java/com/ogong/pms/handler/AbstractStudyHandler.java
@@ -17,10 +17,11 @@ public abstract class AbstractStudyHandler implements Command {
 
   protected void printStudyList() {
     for (Study study : studyList) {
-      System.out.printf(" (%d)\n 스터디명 : %s\n 분류 : %s\n 인원수 : %s명\n 조장 : %s\n 대면/비대면 : %s\n",
+      System.out.printf(" (%d)\n 스터디명 : %s\n 분류 : %s\n 인원수 : %s/%s명\n 조장 : %s\n 대면/비대면 : %s\n",
           study.getStudyNo(),
           study.getStudyTitle(),
           study.getSubject(),
+          study.getMembers().size() + 1,
           study.getNumberOfPeple(),
           study.getOwner().getPerNickname(),
           study.getFace());
@@ -47,7 +48,7 @@ public abstract class AbstractStudyHandler implements Command {
     System.out.printf(" >> 조장 : %s\n", study.getOwner().getPerNickname());
     System.out.printf(" >> 분야 : %s\n", study.getSubject());
     System.out.printf(" >> 지역 : %s\n", study.getArea());
-    System.out.printf(" >> 인원수 : %d\n", study.getNumberOfPeple());
+    System.out.printf(" >> 인원수 : %s/%s명\n", study.getMembers().size() + 1, study.getNumberOfPeple());
     System.out.printf(" >> 대면 : %s\n", study.getFace());
     System.out.printf(" >> 소개글 : %s\n", study.getIntroduction());
 
@@ -96,7 +97,7 @@ public abstract class AbstractStudyHandler implements Command {
         System.out.printf(" >> 조장 : %s\n", searchStudy.getOwner().getPerNickname());
         System.out.printf(" >> 분야 : %s\n", searchStudy.getSubject());
         System.out.printf(" >> 지역 : %s\n", searchStudy.getArea());
-        System.out.printf(" >> 인원수 : %d\n", searchStudy.getNumberOfPeple());
+        System.out.printf(" >> 인원수 : %s/%s명\n", searchStudy.getMembers().size() + 1, searchStudy.getNumberOfPeple());
         System.out.printf(" >> 대면 : %s\n", searchStudy.getFace());
         System.out.printf(" >> 소개글 : %s\n", searchStudy.getIntroduction());
         count++;
--- app/src/main/java/com/ogong/pms/handler/MyStudyDetailHandler.java
+++ app/src/main/java/com/ogong/pms/handler/MyStudyDetailHandler.java
@@ -54,7 +54,7 @@ public class MyStudyDetailHandler extends AbstractStudyHandler {
     System.out.printf(" >> 조장 : %s\n", study.getOwner().getPerNickname());
     System.out.printf(" >> 분야 : %s\n", study.getSubject());
     System.out.printf(" >> 지역 : %s\n", study.getArea());
-    System.out.printf(" >> 인원수 : %d\n", study.getNumberOfPeple());
+    System.out.printf(" >> 인원수 : %s/%s명\n", study.getMembers().size() + 1, study.getNumberOfPeple());
     System.out.printf(" >> 대면 : %s\n", study.getFace());
     System.out.printf(" >> 소개글 : %s\n", study.getIntroduction());
 
