375d784bb3208985506da4f64c47301d1083cd72
woohyeongminn <gudals.woo@gmail.com>
2021-10-03 22:11:47 +0900

    1003 push 회원가입시 닉네임 중복검사 기능 구현

--- app-client/src/main/java/com/ogong/pms/handler/member/MemberAddHandler.java
+++ app-client/src/main/java/com/ogong/pms/handler/member/MemberAddHandler.java
@@ -43,7 +43,7 @@ public class MemberAddHandler implements Command {
         return;
       }
     }
-    String inputNewNick = Prompt.inputString(" 닉네임 : ");
+    inputNewNick = Prompt.inputString(" 닉네임 : ");
     member.setPerNickname(inputNewNick);
     member.setPerPhoto(Prompt.inputString(" 사  진 : "));
 
