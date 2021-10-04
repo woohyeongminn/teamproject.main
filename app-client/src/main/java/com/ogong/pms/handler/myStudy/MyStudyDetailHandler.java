30cfb68d2ef2e519efa93bced46900f9ba930263
woohyeongminn <gudals.woo@gmail.com>
2021-10-04 19:19:38 +0900

    1004 push myStudy 캘린더, 투두, 자유게시판 제외 모두 구현

--- app-client/src/main/java/com/ogong/pms/handler/myStudy/MyStudyDetailHandler.java
+++ app-client/src/main/java/com/ogong/pms/handler/myStudy/MyStudyDetailHandler.java
@@ -32,6 +32,7 @@ public class MyStudyDetailHandler implements Command {
     params.put("studyNo", String.valueOf(inputNo));
 
     requestAgent.request("study.my.selectOneByNicknameStudyNo", params);
+
     Study s = new Study();
     if (requestAgent.getStatus().equals(RequestAgent.SUCCESS)) {
       Study myStudy = requestAgent.getObject(Study.class);
@@ -53,48 +54,48 @@ public class MyStudyDetailHandler implements Command {
       return;
     }
 
-    if (member.getPerNickname().equals(s.getOwner().getPerNickname())) {
-      System.out.println("\n----------------------");
-      System.out.println("1. 구성원");
-      // 대기중인 회원 거절하기
-      // 대기중인 회원 승인할때 회원 고유번호로 입력하도록
-      // 조장 : 권한 넘겨주기, 탈퇴시키기 까지
-      System.out.println("2. 캘린더");
-      System.out.println("3. To-do");
-      System.out.println("4. 자유게시판");
-      System.out.println("5. 화상미팅");
-      System.out.println("6. 탈퇴하기");  // 메서드
-      // 스터디 삭제는 되는데 탈퇴하기가 없음
-      // 구성원은 탈퇴만 해야함, 조장은 스터디삭제도 할수 있음
+    //if (member.getPerNickname().equals(s.getOwner().getPerNickname())) {
+    System.out.println("\n----------------------");
+    System.out.println("1. 구성원");
+    // 대기중인 회원 거절하기
+    // 대기중인 회원 승인할때 회원 고유번호로 입력하도록
+    // 조장 : 권한 넘겨주기, 탈퇴시키기 까지
+    System.out.println("2. 캘린더");
+    System.out.println("3. To-do");
+    System.out.println("4. 자유게시판");
+    System.out.println("5. 화상미팅");
+    System.out.println("6. 탈퇴하기");  // 메서드
+    // 스터디 삭제는 되는데 탈퇴하기가 없음
+    // 구성원은 탈퇴만 해야함, 조장은 스터디삭제도 할수 있음
 
-      if (s.getOwner().getPerNickname().equals(
-          AuthPerMemberLoginHandler.loginUser.getPerNickname())) {
-        System.out.println("7. 스터디 수정");
-        System.out.println("8. 스터디 삭제");
-      }
-      System.out.println("0. 뒤로 가기");
+    if (s.getOwner().getPerNickname().equals(
+        AuthPerMemberLoginHandler.loginUser.getPerNickname())) {
+      System.out.println("7. 스터디 수정");
+      System.out.println("8. 스터디 삭제");
+    }
+    System.out.println("0. 뒤로 가기");
 
-      request.setAttribute("inputNo", s.getStudyNo());
+    request.setAttribute("inputNo", s.getStudyNo());
 
-      int selectNo = Prompt.inputInt("선택> "); 
-      switch (selectNo) {
-        case 1: request.getRequestDispatcher("/myStudy/guilder").forward(request); return;
-        //case 2: myStudyCalender.listCalender(myStudy); break;
-        //case 3: myStudyToDo.listToDo(myStudy); break;
-        //case 4: myStudyFreeBoard.listFreeBoard(commentList, myStudy); break;
-        //case 5:  MyStudyCheating.cheat() ;  // 임시로 넣었음
-        case 6: request.getRequestDispatcher("/myStudy/exit").forward(request); return;  
-        case 7: request.getRequestDispatcher("/myStudy/update").forward(request); return;
-        case 8: request.getRequestDispatcher("/myStudy/delete").forward(request); return;
-        default : return;
-      }
-
+    int selectNo = Prompt.inputInt("선택> "); 
+    switch (selectNo) {
+      case 1: request.getRequestDispatcher("/myStudy/guilder").forward(request); return;
+      //case 2: myStudyCalender.listCalender(myStudy); break;
+      //case 3: myStudyToDo.listToDo(myStudy); break;
+      //case 4: myStudyFreeBoard.listFreeBoard(commentList, myStudy); break;
+      //case 5:  MyStudyCheating.cheat() ;  // 임시로 넣었음
+      case 6: request.getRequestDispatcher("/myStudy/exit").forward(request); return;  
+      case 7: request.getRequestDispatcher("/myStudy/update").forward(request); return;
+      case 8: request.getRequestDispatcher("/myStudy/delete").forward(request); return;
+      default : return;
     }
 
-    //    else if (member.getPerNickname().equals(myStudy.getOwner().getPerNickname()) || 
-    //        myStudy.getMemberNames().contains(member.getPerNickname())) {
+    //}
+
+    //    else if (member.getPerNickname().equals(s.getOwner().getPerNickname()) || 
+    //        s.getMemberNames().contains(member.getPerNickname())) {
     //
-    //      System.out.printf(" \n(%s)\n", myStudy.getStudyNo());
+    //      System.out.printf(" \n(%s)\n", s.getStudyNo());
     //      System.out.printf(" [%s]\n", myStudy.getStudyTitle());
     //      System.out.printf(" >> 조장 : %s\n", myStudy.getOwner().getPerNickname());
     //      System.out.printf(" >> 분야 : %s\n", myStudy.getSubject());
@@ -161,32 +162,7 @@ public class MyStudyDetailHandler implements Command {
     //          return;
     //        }
     //      }
-    //
-    //}
-    //  MyStudyToDo myStudyToDo;
-    //  MyStudyCalender myStudyCalender;
-    //  MyStudyFreeBoard myStudyFreeBoard;
-    //  MyStudyGuilder myStudyGuilder;
-    //  MyStudyExit myStudyExit;
-    //  List<Comment> commentList;
-    //  PromptStudy promptStudy;
-    //
-    //  public MyStudyDetailHandler(List<Study> studyList, MyStudyToDo myStudyToDo, 
-    //      MyStudyCalender myStudyCalender, MyStudyFreeBoard myStudyFreeBoard,
-    //      MyStudyGuilder myStudyGuilder, MyStudyExit myStudyExit, List<Comment> commentList, 
-    //      PromptStudy promptStudy) {
-    //    super(studyList);
-    //    this.myStudyToDo = myStudyToDo;
-    //    this.myStudyCalender = myStudyCalender;
-    //    this.myStudyFreeBoard = myStudyFreeBoard;
-    //    this.myStudyGuilder = myStudyGuilder;
-    //    this.myStudyExit = myStudyExit;
-    //    this.commentList = commentList;
-    //    this.promptStudy = promptStudy;
-    //  }
-
-
-
+    //    }
   }
 
 }
