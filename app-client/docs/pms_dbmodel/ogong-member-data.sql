ab57ca6d181327f4cae1a151bd2792aecd450be4
jo-sol <zoixssm@gmail.com>
2021-10-19 14:02:46 +0900

    1019 push data파일 분리

--- app-client/docs/pms_dbmodel/ogong-member-data.sql
+++ app-client/docs/pms_dbmodel/ogong-member-data.sql
@@ -1,9 +1,3 @@
--- 관리자 test 값 
-insert into admin(admin_no, email, password, nickname) 
-values(1, 'ogong@test.com', password('1234'), '오늘의공부');
-insert into admin(admin_no, email, password, nickname) 
-values(2, 'admin@test.com', password('1234'), '관리자테스트');
-
 -- 회원 test 값
 insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(1, '김초보', '초보초보쌩초보', 'naver@test.com', password('n1'), '010-1111-1111',
@@ -66,47 +60,4 @@ values(3, 9, '우사장', '546-85-14545');
 
 insert into ceo_member(ceo_member_no, member_no, bossname, license_no) 
 values(4, 10, '조사장', '333-28-15464');
-
--------------------------------------------------------------------------------------------------
-
--- 스터디 과목 값
-insert into study_subject(subject_no, name) values(1, '어학');
-insert into study_subject(subject_no, name) values(2, '자격증');
-insert into study_subject(subject_no, name) values(3, '취업');
-insert into study_subject(subject_no, name) values(4, 'IT');
-insert into study_subject(subject_no, name) values(5, '예체능');
-insert into study_subject(subject_no, name) values(6, '기타');
-
--- 대면 상태 값
-insert into study_face_status(face_no, name) values(1, '대면');
-insert into study_face_status(face_no, name) values(2, '비대면');
-insert into study_face_status(face_no, name) values(3, '대면/비대면');
-
-
--- 스터디그룹 test 값
-insert into study(
-study_no, name, subject_no, no_people, face_no, introduction, created_dt, per_member_no, score) 
-values(1, '삼성뿌셔', 3, 5, 3, '취업 뿌셔뿌셔', '2021-1-1', 1, 0);
-
-insert into study(
-study_no, name, subject_no, no_people, face_no, introduction, created_dt, per_member_no, score) 
-values(2, '정처기준비', 2, 5, 2, '한 번에 붙자', '2021-2-2', 1, 0);
-
-
--------------------------------------------------------------------------------------------------
--- 공지사항 test 값
-insert into notice(notice_no, title, content, create_dt)
-values(1, '오늘의 공부 회원 가입을 환영합니다!',
-'오늘의 공부와 함께 목표를 향해 으쌰으쌰!', '2020-01-01');
-insert into notice(notice_no, title, content, create_dt)
-values(2, '오늘의 공부 홈페이지 이용 방법',
-'내용 업데이트 예정입니다.', '2020-02-03');
-
--- 공지사항 test 값 >> 첨부파일
-insert into notice_file(notice_file_no, notice_no, filepath)
-values(1, 1, 'null');
-insert into notice_file(notice_file_no, notice_no, filepath)
-values(2, 2, 'null');
-
-
 
---
+++ app-client/docs/pms_dbmodel/ogong-study-data.sql
@@ -0,0 +1,27 @@
+-- 스터디 과목 값
+insert into study_subject(subject_no, name) values(1, '어학');
+insert into study_subject(subject_no, name) values(2, '자격증');
+insert into study_subject(subject_no, name) values(3, '취업');
+insert into study_subject(subject_no, name) values(4, 'IT');
+insert into study_subject(subject_no, name) values(5, '예체능');
+insert into study_subject(subject_no, name) values(6, '기타');
+
+-- 대면 상태 값
+insert into study_face_status(face_no, name) values(1, '대면');
+insert into study_face_status(face_no, name) values(2, '비대면');
+insert into study_face_status(face_no, name) values(3, '대면/비대면');
+
+
+-- 스터디그룹 test 값
+insert into study(
+study_no, name, subject_no, no_people, face_no, introduction, created_dt, per_member_no, score) 
+values(1, '삼성뿌셔', 3, 5, 3, '취업 뿌셔뿌셔', '2021-1-1', 1, 0);
+
+insert into study(
+study_no, name, subject_no, no_people, face_no, introduction, created_dt, per_member_no, score) 
+values(2, '정처기준비', 2, 5, 2, '한 번에 붙자', '2021-2-2', 1, 0);
+
+
+-------------------------------------------------------------------------------------------------
+
+
