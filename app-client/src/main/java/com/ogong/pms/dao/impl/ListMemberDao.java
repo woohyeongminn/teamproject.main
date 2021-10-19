2474b8cf2d78d99a36027104dde947f3ca470369
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-20 02:40:50 +0900

    1020 push ListMemberDao/NetMemberDao 주석 처리

--- app-client/src/main/java/com/ogong/pms/dao/impl/ListMemberDao.java
+++ app-client/src/main/java/com/ogong/pms/dao/impl/ListMemberDao.java
@@ -1,76 +1,76 @@
-package com.ogong.pms.dao.impl;
-
-import java.util.ArrayList;
-import java.util.List;
-import com.ogong.pms.dao.MemberDao;
-import com.ogong.pms.domain.Member;
-
-public class ListMemberDao implements MemberDao {
-  List<Member> list = new ArrayList<>(); 
-
-  @Override
-  public void insert(Member member) throws Exception {
-    list.add(member);
-  }
-
-  @Override
-  public List<Member> findAll() throws Exception {
-    return list;
-  }
-
-  @Override
-  public Member findByNo(int no) throws Exception {
-    for (Member m : list) {
-      if (m.getPerNo() == no) {
-        return m;
-      }
-    }
-    return null;
-  }
-
-  @Override
-  public Member findByNickName(String inputNick) throws Exception {
-    for (Member m : list) {
-      if (m.getPerNickname().equals(inputNick)) {
-        return m;
-      }
-    }
-    return null;
-  }
-
-  @Override
-  public Member findByEmail(String inputEmail) throws Exception {
-    for (Member m : list) {
-      if (m.getPerEmail().equals(inputEmail)) {
-        return m;
-      }
-    }
-    return null;
-  }
-
-  @Override
-  public Member findByEmailAndPassword(String email, String password) throws Exception {
-    // TODO Auto-generated method stub
-    return null;
-  }
-
-  @Override
-  public void update(Member member) throws Exception {
-    for (int i = 0; i < list.size(); i++) {
-      if (list.get(i).getPerNo() == member.getPerNo()) {
-        list.set(i, member);
-        return;
-      }
-    }
-  }
-
-  @Override
-  public void delete(int no) throws Exception {
-    for (int i = 0; i < list.size(); i++) {
-      if (list.get(i).getPerNo() == no) {
-        list.remove(i);
-        return;
-      }
-    }
-  }
-}
+//package com.ogong.pms.dao.impl;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import com.ogong.pms.dao.MemberDao;
+//import com.ogong.pms.domain.Member;
+//
+//public class ListMemberDao implements MemberDao {
+//  List<Member> list = new ArrayList<>(); 
+//
+//  @Override
+//  public void insert(Member member) throws Exception {
+//    list.add(member);
+//  }
+//
+//  @Override
+//  public List<Member> findAll() throws Exception {
+//    return list;
+//  }
+//
+//  @Override
+//  public Member findByNo(int no) throws Exception {
+//    for (Member m : list) {
+//      if (m.getPerNo() == no) {
+//        return m;
+//      }
+//    }
+//    return null;
+//  }
+//
+//  @Override
+//  public Member findByNickName(String inputNick) throws Exception {
+//    for (Member m : list) {
+//      if (m.getPerNickname().equals(inputNick)) {
+//        return m;
+//      }
+//    }
+//    return null;
+//  }
+//
+//  @Override
+//  public Member findByEmail(String inputEmail) throws Exception {
+//    for (Member m : list) {
+//      if (m.getPerEmail().equals(inputEmail)) {
+//        return m;
+//      }
+//    }
+//    return null;
+//  }
+//
+//  @Override
+//  public Member findByEmailAndPassword(String email, String password) throws Exception {
+//    // TODO Auto-generated method stub
+//    return null;
+//  }
+//
+//  @Override
+//  public void update(Member member) throws Exception {
+//    for (int i = 0; i < list.size(); i++) {
+//      if (list.get(i).getPerNo() == member.getPerNo()) {
+//        list.set(i, member);
+//        return;
+//      }
+//    }
+//  }
+//
+//  @Override
+//  public void delete(int no) throws Exception {
+//    for (int i = 0; i < list.size(); i++) {
+//      if (list.get(i).getPerNo() == no) {
+//        list.remove(i);
+//        return;
+//      }
+//    }
+//  }
+//}
