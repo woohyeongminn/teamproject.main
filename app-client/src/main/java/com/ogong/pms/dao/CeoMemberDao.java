8ea0e3ffefa104bbe502f0c877000ef3c7d842e0
Kimeunchaee <eun00218@naver.com>
2021-10-20 00:29:58 +0900

    1020 push ceoMember DBMS 적용중

--- app-client/src/main/java/com/ogong/pms/dao/CeoMemberDao.java
+++ app-client/src/main/java/com/ogong/pms/dao/CeoMemberDao.java
@@ -6,14 +6,23 @@ import com.ogong.pms.domain.CeoMember;
 public interface CeoMemberDao {
 
   List<CeoMember> findAll() throws Exception;
+
   CeoMember findByNo(int no) throws Exception;
   CeoMember findByNickName(String inputNick) throws Exception;
   CeoMember findByEmail(String inputEmail) throws Exception;
   CeoMember findByEmailAndPassword(String inputEmail, String inputPassword) throws Exception;
+
   void insert(CeoMember ceoMember) throws Exception;
+
   void updateName(CeoMember ceoMember) throws Exception;
   void updateNickName(CeoMember ceoMember) throws Exception;
+  void updatePhoto(CeoMember ceoMember) throws Exception;
+  void updateTel(CeoMember ceoMember) throws Exception;
   void updateEmail(CeoMember ceoMember) throws Exception;
+  void updatePassword(CeoMember ceoMember) throws Exception;
+
+  void updateActive(CeoMember ceoMember) throws Exception;
+
   void delete(int no) throws Exception;
 
 }
