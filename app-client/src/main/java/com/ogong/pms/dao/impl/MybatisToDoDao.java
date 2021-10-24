ec21368b1a1b614aeaf088cb3a8096bc5a72a0dc
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-25 03:48:00 +0900

    1025 push MybatisToDoDao 이름 변경

--- app-client/src/main/java/com/ogong/pms/dao/impl/MybatisToDoDao.java
+++ app-client/src/main/java/com/ogong/pms/dao/impl/MybatisToDoDao.java
@@ -6,12 +6,12 @@ import org.apache.ibatis.session.SqlSession;
 import com.ogong.pms.dao.ToDoDao;
 import com.ogong.pms.domain.ToDo;
 
-public class MybatisTodoDao implements ToDoDao {
+public class MybatisToDoDao implements ToDoDao {
 
   Connection con;
   SqlSession sqlSession;
 
-  public MybatisTodoDao(SqlSession sqlSession) {
+  public MybatisToDoDao(SqlSession sqlSession) {
     this.sqlSession = sqlSession;
   }
 
