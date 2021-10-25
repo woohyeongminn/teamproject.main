b5bce4c19cf87c6acdc60748e74638743f7c4c44
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-10-25 20:24:20 +0900

    1025 push to-do 폴더 및 211025 파일 생성

--- app-client - 복사본/docs/pms_dbmodel/ogong-guilder-data.sql
+++
@@ -1,6 +0,0 @@
--- 스터디구성원 test 값
-insert into study_guilder(member_no,study_no,created_dt,status) 
-values(1,1,'2021-10-25',2);
-
-
-insert into study_guilder(member_no, study_no, status) values(2, 1, 2);
