3d1b54a412ebec0f1b36ab7690f28056c001a0b2
Kimeunchaee <eun00218@naver.com>
2021-10-25 20:24:40 +0900

    1025 commit 자유게시판 자동완성 적용 중

--- app-client/src/main/java/com/ogong/pms/dao/FreeBoardDao.java
+++ app-client/src/main/java/com/ogong/pms/dao/FreeBoardDao.java
@@ -5,11 +5,11 @@ import com.ogong.pms.domain.FreeBoard;
 import com.ogong.pms.domain.FreeBoardFile;
 
 public interface FreeBoardDao {
+  List<FreeBoard> findAll(int studyNo) throws Exception;
+  FreeBoard findByNo(int boardNo, int studyNo) throws Exception;
   void insert(FreeBoard freeBoard) throws Exception;
   void insertFile(FreeBoardFile fileName, int boardNo) throws Exception;
   void update(FreeBoard freeBoard, int studyNo) throws Exception;
-  void delete(int freeBoardNo, int studyNo) throws Exception;
   void deleteFile(int freeBoardNo/* , int fileNo */) throws Exception;
-  List<FreeBoard> findAll(int studyNo) throws Exception;
-  FreeBoard findByNo(int boardNo, int studyNo) throws Exception;
+  void delete(int freeBoardNo, int studyNo) throws Exception;
 }
