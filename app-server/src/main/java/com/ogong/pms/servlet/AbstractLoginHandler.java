74ab8b52d00b97455300778b2f65068d800113be
woohyeongminn <gudals.woo@gmail.com>
2021-10-28 00:24:21 +0900

    1028 pull용 커밋

--- app-server/src/main/java/com/ogong/pms/servlet/AbstractLoginHandler.java
+++ app-server/src/main/java/com/ogong/pms/servlet/AbstractLoginHandler.java
@@ -1,8 +1,9 @@
 package com.ogong.pms.servlet;
 
+import javax.servlet.http.HttpServlet;
 import com.ogong.menu.Menu;
 
-public abstract class AbstractLoginHandler {
+public abstract class AbstractLoginHandler extends HttpServlet {
 
   public static int accessLevel = Menu.LOGOUT;
 
