51959c83401d47ecfec13c5e3a6d5b207335fa1d
Kimeunchaee <eun00218@naver.com>
2021-10-27 20:57:58 +0900

    1027 menu 패키지 추가

--- app-server/src/main/java/com/ogong/pms/servlet/AbstractLoginHandler.java
+++ app-server/src/main/java/com/ogong/pms/servlet/AbstractLoginHandler.java
@@ -1,13 +1,10 @@
 package com.ogong.pms.servlet;
 
+import com.ogong.menu.Menu;
+
 public abstract class AbstractLoginHandler {
 
-  public static final int LOGOUT = 0x01; //유저 로그아웃인 경우
-  public static final int PER_LOGIN = 0x02;  // 개인 로그인인 경우
-  public static final int CEO_LOGIN = 0x04;  // 기업회원 로그인인 경우
-  public static final int ADMIN_LOGIN = 0x08;  // 관리자 로그인인 경우
-
-  public static int accessLevel = LOGOUT;
+  public static int accessLevel = Menu.LOGOUT;
 
   public static int getUserAccessLevel() {
     return accessLevel;
