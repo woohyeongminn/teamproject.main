51b382e9c75f2a27416b7512042e3db85b1a8b48
Kimeunchaee <eun00218@naver.com>
2021-10-29 16:53:45 +0900

    1029 자유게시판 테스트값에 고유번호 추가함

--- app-client/docs/pms_dbmodel/ogong-FreeBoard-data.sql
+++ app-client/docs/pms_dbmodel/ogong-FreeBoard-data.sql
@@ -1,26 +1,26 @@
 
 --스터디 조장, 구성원 테스트값 보면서 넣어야함
 
-insert into study_board (title, content, view_ct, created_dt, member_no, study_no)
-values ('제목1', '내용1입니다.', '2', '2021-10-5', '1' , '1');
+insert into study_board (st_board_no, title, content, view_ct, created_dt, member_no, study_no)
+values (1, '제목1', '내용1입니다.', '2', '2021-10-5', '1' , '1');
 
-insert into study_board (title, content, view_ct, created_dt, member_no, study_no)
-values ('제목2', '내용2입니다.', '0', '2021-10-10', '3' , '1');
+insert into study_board (st_board_no, title, content, view_ct, created_dt, member_no, study_no)
+values (2, '제목2', '내용2입니다.', '0', '2021-10-10', '3' , '1');
 
-insert into study_board (title, content, view_ct, created_dt, member_no, study_no)
-values ('제목3', '내용3입니다.', '0', '2021-10-20', '5' , '1');
+insert into study_board (st_board_no, title, content, view_ct, created_dt, member_no, study_no)
+values (3, '제목3', '내용3입니다.', '0', '2021-10-20', '5' , '1');
 
-insert into study_board (title, content, view_ct, created_dt, member_no, study_no)
-values ('제목4', '내용4입니다.', '0', '2021-09-01', '2' , '1');
+insert into study_board (st_board_no, title, content, view_ct, created_dt, member_no, study_no)
+values (4, '제목4', '내용4입니다.', '0', '2021-09-01', '2' , '1');
 
-insert into study_board (title, content, view_ct, created_dt, member_no, study_no)
-values ('제목5', '내용5입니다.', '0', '2021-09-01', '4' , '1');
+insert into study_board (st_board_no, title, content, view_ct, created_dt, member_no, study_no)
+values (5, '제목5', '내용5입니다.', '0', '2021-09-01', '4' , '1');
 
-insert into study_board (title, content, view_ct, created_dt, member_no, study_no, whether_report)
-values ('제목6', '내용6입니다.', '0', '2021-09-01', '6' , '1', '2');
+insert into study_board (st_board_no, title, content, view_ct, created_dt, member_no, study_no, whether_report)
+values (6, '제목6', '내용6입니다.', '0', '2021-09-01', '6' , '1', '2');
 
-insert into study_board (title, content, view_ct, created_dt, member_no, study_no, whether_report)
-values ('제목7', '내용7입니다.', '0', '2021-09-01', '7' , '1', '2');
+insert into study_board (st_board_no, title, content, view_ct, created_dt, member_no, study_no, whether_report)
+values (7, '제목7', '내용7입니다.', '0', '2021-09-01', '7' , '1', '2');
 
 -- 목록
 /*
