2cc990d1a7295d53051bc30c547a88c2dbf09d31
ssongdahye <songdh121@gmail.com>
2021-10-29 16:42:42 +0900

    1029 push ogong-cafe-data.sql 수정

--- app-client/docs/pms_dbmodel/ogong-cafe-data.sql
+++ app-client/docs/pms_dbmodel/ogong-cafe-data.sql
@@ -11,16 +11,16 @@ values (4, '삭제');
 -- 카페
 insert into studycafe(cafe_no, name, info, location, phone, open_time, close_time, operating_status_no, member_no)
 values (1, '에이스터디카페', '스터디 모임 전문 공간 에이스터디카페 입니다.', 
-'서울 강남구 강남대로94길 11 맨하탄어학원빌딩 4층', '02-111-1111', '08:00', '21:00', 2, 7);
+'서울 강남구 강남대로94길 11 맨하탄어학원빌딩 4층', '02-111-1111', '08:00', '21:00', 2, 8);
 insert into studycafe(cafe_no, name, info, location, phone, open_time, close_time, operating_status_no, member_no)
 values (2, '해피해피스터디카페', '안녕하세요! 강남에 위치한 해피해피스터디카페 입니다.', 
-'서울 강남구 강남대로92길 13 5층', '02-123-1234', '07:00', '23:00', 2, 8);
+'서울 강남구 강남대로92길 13 5층', '02-123-1234', '07:00', '23:00', 2, 9);
 insert into studycafe(cafe_no, name, info, location, phone, open_time, close_time, operating_status_no, member_no)
 values (3, '광명스터디카페', '공부가 잘 되는 공간, 광명스터디카페 입니다.', 
-'경기 광명시 철산로 12 2층 2호', '010-5555-5555', '06:00', '22:00', 2, 9);
+'경기 광명시 철산로 12 2층 2호', '010-5555-5555', '06:00', '22:00', 2, 10);
 insert into studycafe(cafe_no, name, info, location, phone, open_time, close_time, operating_status_no, member_no)
 values (4, '비트스터디카페 판교센터', '안녕하세요! 비트스터디카페 판교센터 입니다.', 
-'경기 성남시 분당구 서판교로 36 나이스빌딩 4층 402호', '010-5555-5555', '05:00', '23:00', 1, 10);
+'경기 성남시 분당구 서판교로 36 나이스빌딩 4층 402호', '010-5555-5555', '05:00', '23:00', 1, 11);
 
 -- 카페 사진
 insert into studycafe_photo(photo_no, name, cafe_no)
