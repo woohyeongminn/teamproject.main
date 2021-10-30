2cf7453111e03de795f16ecbd0a41a388ba4cec4
woohyeongminn <gudals.woo@gmail.com>
2021-10-30 16:19:47 +0900

    1030 push member jsp 적용중 (로그인,상세 구현)

--- app-server/src/main/java/com/ogong/pms/servlet/member/MemberUpdateHandler.java
+++ app-server/src/main/java/com/ogong/pms/servlet/member/MemberUpdateHandler.java
@@ -1,11 +1,13 @@
 package com.ogong.pms.servlet.member;
 
 import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.apache.ibatis.session.SqlSession;
 import com.ogong.pms.dao.MemberDao;
 import com.ogong.pms.domain.Member;
@@ -14,162 +16,57 @@ import com.ogong.pms.domain.Member;
 public class MemberUpdateHandler extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
-  MemberDao memberDao;
   SqlSession sqlSession;
+  MemberDao memberDao;
 
   @Override
-  public void service(ServletRequest request, ServletResponse response)
+  public void init(ServletConfig config) throws ServletException {
+    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
+    sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
+    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
+  }
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    //String perName = member.getPerName();
-    //String perNickName = member.getPerNickname();
-    //String perPhoto = member.getPerPhoto();
-    //String perTel = member.getPerTel();
-    //String perEmail = member.getPerEmail();
-    //String perPassword = member.getPerPassword();
-
     try {
-      int no = (int) request.getAttribute("memberNo");
-      //List<Member> memberList = memberDao.findAll();
+      int no = Integer.parseInt(request.getParameter("no"));
       Member member = memberDao.findByNo(no);
-      request.setAttribute("updateMember", member);
-      request.getRequestDispatcher("MemberUpdate.jsp").forward(request, response);
-    } catch (Exception e) {
 
+      member.setPerNickname("nickname");
+      member.setPerName(request.getParameter("name"));
+      member.setPerEmail(request.getParameter("email"));
+      member.setPerPassword(request.getParameter("password"));
+      member.setPerPhoto(request.getParameter("photo"));
+      member.setPerTel(request.getParameter("tel"));
+
+      memberDao.updateName(member);
+      memberDao.updateNickname(member);
+      memberDao.updateEmail(member);
+      memberDao.updatePassword(member);
+      memberDao.updatePhoto(member);
+      memberDao.updateTel(member);
+      sqlSession.commit();
+
+      response.sendRedirect("list");
+
+    }catch (Exception e) {
+      request.setAttribute("error", e);
+      request.getRequestDispatcher("/Error.jsp").forward(request, response);
     }
-    //
-    //    switch (selectNo) {
-    //      case 1:
-    //        LOOP: while (true) {
-    //          perName = Prompt.inputString(" 이  름(" + member.getPerName() + ") : ");
-    //          for (Member preMemberName : memberList) {
-    //            if (perName.equals(preMemberName.getPerName())) {
-    //              System.out.println(" >> 이미 사용 중인 이름입니다.");
-    //              continue LOOP;
-    //            }
-    //          }
-    //          break;
-    //        }
-    //        break;
-    //
-    //      case 2:
-    //        perNickName = Prompt.inputString(" 닉네임(" + member.getPerNickname() + ") : ");
-    //        while (true) {
-    //          for (Member comparisonMember : memberList) {
-    //            if (perNickName.equals(comparisonMember.getPerNickname())) {
-    //              System.out.println(" >> 이미 사용중인 닉네임입니다.");
-    //              continue;
-    //            }
-    //          }
-    //          break;
-    //        }
-    //        break;
-    //
-    //      case 3:
-    //        perPhoto = Prompt.inputString(" 사  진(" + member.getPerPhoto() + ") : ");
-    //        break;
-    //
-    //      case 4:
-    //        LOOP: while (true) {
-    //          perTel = Prompt.inputString(" 전화번호(" + member.getPerTel() + ") : ");
-    //          // [삭제] 입력 형식 불명확
-    //          // if (perTel.length() > 10 && perTel.length() < 11) {
-    //          // System.out.println(" >> 올바른 형식의 전화번호를 입력해 주세요.");
-    //          // continue;
-    //          // }
-    //
-    //          for (Member perMemberTel : memberList) {
-    //            if (perTel.equals(perMemberTel.getPerTel())) {
-    //              System.out.println(" >> 이미 사용 중인 전화번호입니다.");
-    //              continue LOOP;
-    //            }
-    //          }
-    //          break;
-    //        }
-    //        break;
-    //
-    //      case 5:
-    //        while (true) {
-    //          perEmail = Prompt.inputString(" 이메일(" + member.getPerEmail() + ") : ");
-    //          if (!perEmail.contains("@") || !perEmail.contains(".com") || perEmail.length() < 6) {
-    //            System.out.println(" >> 정확한 이메일 양식으로 입력해 주세요.");
-    //            continue;
-    //          }
-    //          break;
-    //        }
-    //        break;
-    //
-    //      case 6:
-    //        while (true) {
-    //          perPassword = Prompt.inputString(" 변경할 비밀번호 : ");
-    //          if (perPassword.length() < 8 || (!perPassword.contains("!") && !perPassword.contains("@")
-    //              && !perPassword.contains("#") && !perPassword.contains("$")
-    //              && !perPassword.contains("^") && !perPassword.contains("%")
-    //              && !perPassword.contains("&") && !perPassword.contains("*"))) {
-    //            System.out.println(" >> 8자 이상 특수문자를 포함시켜 주세요.");
-    //            continue;
-    //          }
-    //          break;
-    //        }
-    //
-    //        while (true) {
-    //          String pw = Prompt.inputString(" 비밀번호 확인 : ");
-    //          if (!pw.equals(perPassword)) {
-    //            System.out.println("\n >> 확인 실패!");
-    //            continue;
-    //          } else {
-    //            System.out.println("\n >> 확인 완료!");
-    //          }
-    //          break;
-    //        }
-    //        break;
-    //
-    //      case 0: return;
-    //
-    //      default:
-    //        System.out.println(" >> 올바른 번호를 입력해 주세요.");
-    //        return;
-    //    }
-
-    //
-    //    if (selectNo == 1) {
-    //      member.setPerName(perName);
-    //      memberDao.updateName(member);
-    //      sqlSession.commit();
-    //
-    //    } else if (selectNo == 2) {
-    //      member.setPerNickname(perNickName);
-    //      memberDao.updateNickname(member);
-    //      sqlSession.commit();
-    //
-    //    } else if (selectNo == 3) {
-    //      member.setPerPhoto(perPhoto);
-    //      memberDao.updatePhoto(member);
-    //      sqlSession.commit();
-    //
-    //    } else if (selectNo == 4) {
-    //      member.setPerTel(perTel);
-    //      memberDao.updateTel(member);
-    //      sqlSession.commit();
-    //
-    //    } else if (selectNo == 5) {
-    //      member.setPerEmail(perEmail);
-    //      memberDao.updateEmail(member);
-    //      sqlSession.commit();
-    //
-    //    } else if (selectNo == 6) {
-    //      member.setPerPassword(perPassword);
-    //      memberDao.updatePassword(member);
-    //      sqlSession.commit();
-
-    //      AuthPerMemberLoginHandler.loginUser = null;
-    //      AuthPerMemberLoginHandler.accessLevel = Menu.LOGOUT;
-    //      System.out.println("\n >> 회원 정보를 변경하였습니다.\n");
-    //      System.out.println(" >> 다시 로그인 해 주세요.");
-    //      return;
-    //    }
-    //
-    //    System.out.println(" >> 회원 정보를 변경하였습니다.");
-
   }
+
+  @Override
+  public String getServletInfo() {
+    return null;
+  }
+
+  @Override
+  public ServletConfig getServletConfig() {
+    return null;
+  }
+
 }
+
+
---
+++ app-server/src/main/webapp/member/MemberUpdateForm.jsp
@@ -0,0 +1,46 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>회원수정</title>
+  <style>
+  label {
+    margin-right: 5px;
+    text-align: right;
+    display: inline-block;
+    width: 60px;
+  }
+  </style>
+</head>
+<body>
+<h1>회원 수정(test)</h1>
+
+<form action='../member/update'>
+    <label for='f-no'>번호</label> 
+    <input id='f-no' type='text' name='no' value='${updateMember.perNo}' readonly><br>
+    
+    <label for='f-nickname'>닉네임</label>
+    <input id='f-nickname' type='text' name='nickname' value='${updateMember.perNickname}'><br>
+    
+    <label for='f-name'>이름</label>
+    <input id='f-name' type='text' name='name' value='${updateMember.perName}'><br>
+    
+    <label for='f-email'>이메일</label> 
+    <input id='f-email' type='email' name='email' value='${updateMember.perEmail}'><br>
+    
+    <label for='f-password'>암호</label> 
+    <input id='f-password' type='password' name='password'><br>
+    
+    <label for='f-photo'>사진</label> 
+    <input id='f-photo' type='text' name='photo' value='${updateMember.perPhoto}'><br>
+    
+    <label for='f-tel'>전화</label> 
+    <input id='f-tel' type='tel' name='tel' value='${updateMember.perTel}'><br>
+    
+    <label for='f-registeredDate'>등록일</label> 
+    <span id='f-registeredDate'>${updateMember.perRegisteredDate}</span><br>
+<button>변경</button>
+</form>
+</body>
+</html>
