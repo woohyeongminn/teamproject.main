815e993d908c59c1c8f475445625b1ec971fa4b9
jo-sol <zoixssm@gmail.com>
2021-10-31 22:14:31 +0900

    1031 save 푸시 전 커밋 > 공지사항 삭제 추가, 공지사항 목록&업데이트&디테일 수정

--- app-server/src/main/java/com/ogong/pms/servlet/admin/AdminNoticeDeleteController.java
+++ app-server/src/main/java/com/ogong/pms/servlet/admin/AdminNoticeDeleteController.java
@@ -1,7 +1,6 @@
 package com.ogong.pms.servlet.admin;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -14,7 +13,7 @@ import com.ogong.pms.dao.NoticeDao;
 import com.ogong.pms.domain.AdminNotice;
 
 @WebServlet("/adminNotice/delete")
-public class AdminNoticeDeleteHandler extends HttpServlet {
+public class AdminNoticeDeleteController extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   SqlSession sqlSession;
@@ -31,41 +30,25 @@ public class AdminNoticeDeleteHandler extends HttpServlet {
   protected void service(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
 
-    response.setContentType("text/html;charset=UTF-8");
-    PrintWriter out = response.getWriter();
-
-    out.println("<html>");
-    out.println("<head>");
-    out.println("  <title>공지게시판</title>");
-    out.println("</head>");
-    out.println("<body>");
-    out.println("<h1> ▶ 공지 삭제 </h1>");
-    out.println("<hr>");
-
     try {
       int noticeNo = Integer.parseInt(request.getParameter("no"));
 
       AdminNotice notice = noticeDao.findByNoticeNo(noticeNo);
 
       if (notice == null) {
-        out.println(" >> 해당 번호의 공지글이 없습니다.<br>");
-
-      } else {
-
-        noticeDao.deletenoticefile(noticeNo);
-        noticeDao.delete(noticeNo);
-        sqlSession.commit();
-
-        out.println(" >> 공지가 삭제되었습니다.<br>");
+        throw new Exception (" >> 해당 번호의 공지글이 없습니다.<br>");
 
       }
-      out.println("<a href='list'>[목록]<a><br>");
+
+      noticeDao.deletenoticefile(noticeNo);
+      noticeDao.delete(noticeNo);
+      sqlSession.commit();
+
+      response.sendRedirect("list");
 
     } catch (Exception e) {
-      throw new ServletException(e);
+      request.setAttribute("error", e);
+      request.getRequestDispatcher("/Error.jsp").forward(request, response);
     }
-
-    out.println("</body>");
-    out.println("</html>");
   }
 }
