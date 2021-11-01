a91dc57d4d391399df91bcaa29b73bbd695ba0ee
Kimeunchaee <eun00218@naver.com>
2021-11-01 15:57:09 +0900

    Merge branch 'main' of https://github.com/woohyeongminn/teamproject.main.git into main

--- app-server/src/main/java/com/ogong/pms/servlet/ceoMember/CeoLoginFormController.java
+++ app-server/src/main/java/com/ogong/pms/servlet/ceoMember/CeoLoginFormController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/ceomember/loginform")
+@WebServlet("/ceomember/form")
 public class CeoLoginFormController extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
