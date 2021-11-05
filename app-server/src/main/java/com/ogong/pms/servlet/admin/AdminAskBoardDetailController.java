70c530e7aa7ecada869019310a9a08723db38555
woohyeongminn <gudals.woo@gmail.com>
2021-11-05 09:34:15 +0900

    1105 문의게시글 서블릿 이름 변경

--- app-server/src/main/java/com/ogong/pms/servlet/admin/AdminAskBoardDetailController.java
+++ app-server/src/main/java/com/ogong/pms/servlet/admin/AdminAskBoardDetailController.java
@@ -1,19 +1,18 @@
 package com.ogong.pms.servlet.admin;
 
 import java.io.IOException;
-import java.util.Collection;
-import javax.servlet.GenericServlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import com.ogong.pms.dao.AskBoardDao;
 import com.ogong.pms.domain.AskBoard;
 
-@WebServlet("/admin/askboardlist")
-public class AdminAskBoardListHandler extends GenericServlet {
+@WebServlet("/admin/askboarddetail")
+public class AdminAskBoardDetailController extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   AskBoardDao askBoardDao;
@@ -29,17 +28,16 @@ public class AdminAskBoardListHandler extends GenericServlet {
       throws ServletException, IOException {
 
     try {
-      Collection<AskBoard> adminAskBoardList = askBoardDao.findAll();
+      int askBoardNo = Integer.parseInt(request.getParameter("askNo"));
+      AskBoard adminAskBoard = askBoardDao.findByNo(askBoardNo);
 
-      if (adminAskBoardList == null) {
-        throw new Exception(" >> 등록된 글이 없습니다.");
+      if (adminAskBoard == null) {
+        throw new Exception("해당 번호의 게시글이 없습니다.");
       }
-
-      request.setAttribute("adminAskBoardList", adminAskBoardList);
-      request.getRequestDispatcher("/admin/AskBoardList.jsp").forward(request, response);   
+      request.setAttribute("adminAskBoard", adminAskBoard);
+      request.getRequestDispatcher("/admin/AskBoardDetail.jsp").forward(request, response);
 
     } catch (Exception e) {
-      e.printStackTrace();
       request.setAttribute("error", e);
       request.getRequestDispatcher("/Error.jsp").forward(request, response);
     }
