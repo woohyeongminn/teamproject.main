cfa4e69696e644075f4edaf2a608f20116b6c0ff
woohyeongminn <gudals.woo@gmail.com>
2021-11-10 17:12:48 +0900

    1110 push memberDao 수정 Member html 디테일 구현 중

--- app-server/src/main/java/com/ogong/pms/servlet/member/MemberUpdateFromController.java
+++ app-server/src/main/java/com/ogong/pms/servlet/member/MemberUpdateFromController.java
@@ -1,41 +1,22 @@
 package com.ogong.pms.servlet.member;
 
 import java.io.IOException;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
-import com.ogong.pms.dao.MemberDao;
-import com.ogong.pms.domain.Member;
 
 @WebServlet("/member/updateform")
 public class MemberUpdateFromController extends HttpServlet {
   private static final long serialVersionUID = 1L;
-
-  MemberDao memberDao;
-
-  @Override
-  public void init(ServletConfig config) throws ServletException {
-    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
-    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
-  }
 
   @Override
   public void service(ServletRequest request, ServletResponse response)
       throws ServletException, IOException {
 
     try {
-      int no = Integer.parseInt(request.getParameter("no"));
-      Member perMember = memberDao.findByNo(no);
 
-      if (perMember == null) {
-        throw new Exception("해당 번호의 회원이 없습니다.");
-      }
-
-      request.setAttribute("perMember", perMember);
       request.getRequestDispatcher("/member/PerMemberUpdateForm.jsp").forward(request, response);
 
     } catch (Exception e) {
