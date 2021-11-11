3995a8b44a22fbca328ca7ffdf1118af6b40babb
Kimeunchaee <eun00218@naver.com>
2021-11-12 03:28:19 +0900

    1112 push ceo프로필사진 추가 / 테스트값 사진명 수정

--- app-client/docs/pms_dbmodel/ogong-ceomember-data.sql
+++ app-client/docs/pms_dbmodel/ogong-ceomember-data.sql
@@ -1,39 +1,39 @@
 -- 회원 test 값
 insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(8, '김사장', '김사장카페', 'cafe1@test.com', password('c1'), '010-1111-0000',
-'cafe1.gif', '2020-02-02', 2);
+'ceoProfile', '2020-02-02', 2);
 
 insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(9, '송사장', '송사장카페', 'cafe2@test.com', password('c2'), '010-2222-0000',
-'cafe2.gif', '2021-04-15', 2);
+'ceoProfile_color', '2021-04-15', 2);
 
 insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(10, '우사장', '우사장카페', 'cafe3@test.com', password('c3'), '010-3333-0000',
-'cafe3.gif','2021-09-16', 2);
+'ceoProfile_color','2021-09-16', 2);
 
 insert into member(member_no, name, nickname, email, password, tel, photo, status) 
 values(11, '조사장', '조사장카페', 'cafe4@test.com', password('c4'), '010-4444-0000', 
-'cafe4.gif', 2);
+'ceoProfile_color', 2);
 
 insert into member(member_no, name, nickname, email, password, tel, photo, status) 
 values(12, '반사장', '반사장카페', 'cafe5@test.com', password('c5'), '010-5555-0000', 
-'cafe5.gif', 2);
+'ceoProfile', 2);
 
 insert into member(member_no, name, nickname, email, password, tel, photo, status) 
 values(13, '이사장', '이사장카페', 'cafe6@test.com', password('c6'), '010-6666-0000', 
-'cafe6.gif', 2);
+'ceoProfile_color', 2);
 
 insert into member(member_no, name, nickname, email, password, tel, photo, status) 
 values(14, '박사장', '박사장카페', 'cafe7@test.com', password('c7'), '010-7777-0000', 
-'cafe7.gif', 2);
+'ceoProfile', 2);
 
 insert into member(member_no, name, nickname, email, password, tel, photo, status) 
 values(15, '한사장', '한사장카페', 'cafe8@test.com', password('c8'), '010-8888-0000', 
-'cafe7.gif', 2);
+'ceoProfile_color', 2);
 
 insert into member(member_no, name, nickname, email, password, tel, photo, status) 
 values(16, '최사장', '최사장카페', 'cafe9@test.com', password('c9'), '010-9999-0000', 
-'cafe7.gif', 2);
+'ceoProfile', 2);
 
 /*
 insert into member(name,nickname,email,password,tel,photo,status)
