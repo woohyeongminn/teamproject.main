0946d58cee90bac65ef72d7c330cfc54275928e6
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-11-13 21:44:17 +0900

    1113 commit FreeBoardAdd 파일 업로드 스프링 적용 중

--- app-server/src/main/java/com/ogong/pms/web/myStudy/freeBoard/FreeBoardAddController.java
+++ app-server/src/main/java/com/ogong/pms/web/myStudy/freeBoard/FreeBoardAddController.java
@@ -1,5 +1,7 @@
 package com.ogong.pms.web.myStudy.freeBoard;
 
+import java.util.UUID;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -11,6 +13,10 @@ import com.ogong.pms.dao.FreeBoardDao;
 import com.ogong.pms.domain.FreeBoard;
 import com.ogong.pms.domain.Member;
 import com.ogong.pms.domain.Study;
+import net.coobird.thumbnailator.ThumbnailParameter;
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Positions;
+import net.coobird.thumbnailator.name.Rename;
 
 @Controller
 public class FreeBoardAddController {
@@ -19,10 +25,34 @@ public class FreeBoardAddController {
   SqlSessionFactory sqlSessionFactory;
   @Autowired
   FreeBoardDao freeBoardDao;
+  @Autowired
+  ServletContext sc;
 
   @PostMapping("/freeboard/add")
   public ModelAndView add(Study study, Part photoFile, HttpSession session) throws Exception {
     FreeBoard freeBoard = new FreeBoard();
+
+    if (photoFile.getSize() > 0) {
+      String filename = UUID.randomUUID().toString();
+      photoFile.write(sc.getRealPath("/upload/member") + "/" + filename);
+      freeBoard.setFreeBoardFile(filename);
+
+      Thumbnails.of(sc.getRealPath("/upload/member") + "/" + filename).size(20, 20)
+      .outputFormat("jpg").crop(Positions.CENTER).toFiles(new Rename() {
+        @Override
+        public String apply(String name, ThumbnailParameter param) {
+          return name + "_20x20";
+        }
+      });
+
+      Thumbnails.of(sc.getRealPath("/upload/member") + "/" + filename).size(100, 100)
+      .outputFormat("jpg").crop(Positions.CENTER).toFiles(new Rename() {
+        @Override
+        public String apply(String name, ThumbnailParameter param) {
+          return name + "_100x100";
+        }
+      });
+    }
 
     freeBoard.setStudyNo(((Study) session.getAttribute("studyno")).getStudyNo());
     freeBoard.setFreeBoardWriter((Member) session.getAttribute("loginUser"));
