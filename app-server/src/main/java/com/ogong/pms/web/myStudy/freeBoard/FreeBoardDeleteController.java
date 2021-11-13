7a41a77629cb86d68b7551d38e1876dd3dfa0703
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-11-13 22:46:02 +0900

    1113 commit FreeBoardDetail 스프링 적용

--- app-server/src/main/java/com/ogong/pms/web/myStudy/freeBoard/FreeBoardDeleteController.java
+++ app-server/src/main/java/com/ogong/pms/web/myStudy/freeBoard/FreeBoardDeleteController.java
@@ -1,63 +1,45 @@
 package com.ogong.pms.web.myStudy.freeBoard;
 
-import java.io.IOException;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 import com.ogong.pms.dao.FreeBoardDao;
 import com.ogong.pms.domain.FreeBoard;
 
-@WebServlet("/freeboard/delete")
-public class FreeBoardDeleteController extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+@Controller
+public class FreeBoardDeleteController {
 
-  // MemberDao memberDao;
+  @Autowired
+  SqlSessionFactory sqlSessionFactory;
+  @Autowired
   FreeBoardDao freeBoardDao;
-  SqlSession sqlSession;
 
-  @Override
-  public void init(ServletConfig config) throws ServletException {
-    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
-    sqlSession = (SqlSession) 웹애플리케이션공용저장소.getAttribute("sqlSession");
-    // memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
-    freeBoardDao = (FreeBoardDao) 웹애플리케이션공용저장소.getAttribute("freeBoardDao");
-  }
+  @GetMapping("/freeboard/delete")
+  public ModelAndView delete(int freeboardno, int studyno) throws Exception {
+    // int studyNo = Integer.parseInt(request.getParameter("studyno"));
+    // int freeBoardNo = Integer.parseInt(request.getParameter("freeboardno"));
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+    FreeBoard freeBoard = freeBoardDao.findByNo(freeboardno, studyno);
 
-    try {
-      // int perNo = Integer.parseInt(request.getParameter("perNo"));
-      int studyNo = Integer.parseInt(request.getParameter("studyno"));
-      int freeBoardNo = Integer.parseInt(request.getParameter("freeboardno"));
-
-      FreeBoard freeBoard = freeBoardDao.findByNo(freeBoardNo, studyNo);
-
-      if (freeBoard == null) {
-        throw new Exception("해당 번호의 게시글이 없습니다.");
-      }
-
-      if (!freeBoard.getFreeBoardFile().isEmpty()) {
-        throw new Exception("게시글의 첨부파일이 모두 삭제됩니다.");
-      }
-
-      freeBoardDao.deleteComment(freeBoardNo);
-      freeBoardDao.deleteFile(freeBoardNo);
-      freeBoardDao.delete(freeBoardNo, studyNo);
-      sqlSession.commit();
-
-      response.sendRedirect("list?studyno=" + studyNo);
-
-    } catch (Exception e) {
-      e.printStackTrace();
-      request.setAttribute("error", e);
-      request.getRequestDispatcher("/Error.jsp").forward(request, response);
+    if (freeBoard == null) {
+      throw new Exception("해당 번호의 자유 게시판이 없습니다.");
     }
+
+    if (!freeBoard.getFreeBoardFile().isEmpty()) {
+      throw new Exception("게시글의 첨부파일이 모두 삭제됩니다.");
+    }
+
+    freeBoardDao.deleteComment(freeboardno);
+    freeBoardDao.deleteFile(freeboardno);
+    freeBoardDao.delete(freeboardno, studyno);
+    sqlSessionFactory.openSession().commit();
+
+    ModelAndView mv = new ModelAndView();
+
+    mv.setViewName("redirect:list?studyno=" + studyno);
+
+    return mv;
   }
 }
