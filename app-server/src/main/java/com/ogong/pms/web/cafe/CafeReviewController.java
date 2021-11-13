a46dec99d5bbe8f8b675edb4aeb2e8892ab5f1ff
ssongdahye <songdh121@gmail.com>
2021-11-13 21:56:07 +0900

    1113 카페리뷰 등록/목록/수정/삭제 스프링 적용

---
+++ app-server/src/main/java/com/ogong/pms/web/cafe/CafeReviewController.java
@@ -0,0 +1,97 @@
+package com.ogong.pms.web.cafe;
+
+import java.util.List;
+import javax.servlet.http.HttpSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.ogong.pms.dao.CafeDao;
+import com.ogong.pms.dao.CafeReservationDao;
+import com.ogong.pms.dao.CafeReviewDao;
+import com.ogong.pms.domain.CafeReview;
+import com.ogong.pms.domain.Member;
+
+@Controller
+public class CafeReviewController {
+
+  @Autowired SqlSessionFactory sqlSessionFactory;
+  @Autowired CafeDao cafeDao;
+  @Autowired CafeReviewDao cafeReviewDao;
+  @Autowired CafeReservationDao cafeReservationDao;
+
+  @PostMapping("/cafe/reviewAdd")
+  public ModelAndView add(CafeReview cafeReview) throws Exception {
+
+    try {
+      cafeReviewDao.insertCafeReview(cafeReview);
+      cafeReservationDao.updateCafeReservationReviewStatus(cafeReview.getReservationNo());
+      sqlSessionFactory.openSession().commit();
+    } catch (Exception e) {
+      sqlSessionFactory.openSession().rollback();
+    }
+
+    ModelAndView mv = new ModelAndView();
+    mv.setViewName("redirect:reviewList");
+
+    return mv;
+  }
+
+  @GetMapping("/cafe/reviewList")
+  public ModelAndView list(HttpSession session) throws Exception {
+
+    Member member = (Member) session.getAttribute("loginUser");
+
+    List<CafeReview> reviewList = cafeReviewDao.findReviewListByMember(member.getPerNo());
+
+    ModelAndView mv = new ModelAndView();
+
+    mv.addObject("reviewList", reviewList);
+    mv.addObject("pageTitle", "내 리뷰 내역");
+    mv.addObject("contentUrl", "cafe/CafeReviewList.jsp");
+    mv.setViewName("template1");
+
+    return mv;
+  }
+
+  @GetMapping("/cafe/reviewUpdateForm")
+  public ModelAndView updateForm(int reviewNo) throws Exception {
+
+    CafeReview cafeReview = cafeReviewDao.findByReviewNo(reviewNo);
+
+    ModelAndView mv = new ModelAndView();
+
+    mv.addObject("cafeReview", cafeReview);
+    mv.addObject("pageTitle", "리뷰 수정");
+    mv.addObject("contentUrl", "cafe/CafeReviewUpdateForm.jsp");
+    mv.setViewName("/template1");
+
+    return mv;
+  }
+
+  @PostMapping("/cafe/reviewUpdate")
+  public ModelAndView update(CafeReview cafeReview) throws Exception {
+
+    cafeReviewDao.updateCafeReview(cafeReview);
+    sqlSessionFactory.openSession().commit();
+
+    ModelAndView mv = new ModelAndView();
+    mv.setViewName("redirect:reviewList");
+
+    return mv;
+  }
+
+  @GetMapping("/cafe/reviewDelete")
+  public ModelAndView delete(int reviewNo) throws Exception {
+
+    cafeReviewDao.deleteCafeReview(reviewNo);
+    sqlSessionFactory.openSession().commit();
+
+    ModelAndView mv = new ModelAndView();
+    mv.setViewName("redirect:reviewList");
+
+    return mv;
+  }
+}
