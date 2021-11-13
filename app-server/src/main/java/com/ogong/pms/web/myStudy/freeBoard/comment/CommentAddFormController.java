285b112a582e4fb65384d241ffa2542cefe27022
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-11-13 23:33:35 +0900

    1113 commit CommentAddForm 스프링 적용

--- app-server/src/main/java/com/ogong/pms/web/myStudy/freeBoard/comment/CommentAddFormController.java
+++ app-server/src/main/java/com/ogong/pms/web/myStudy/freeBoard/comment/CommentAddFormController.java
@@ -1,60 +1,30 @@
 package com.ogong.pms.web.myStudy.freeBoard.comment;
 
-import java.io.IOException;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 import com.ogong.pms.dao.CommentDao;
 import com.ogong.pms.dao.FreeBoardDao;
-import com.ogong.pms.dao.StudyDao;
 import com.ogong.pms.domain.FreeBoard;
-import com.ogong.pms.domain.Study;
 
-@WebServlet("/freeboard/comment/form")
-public class CommentAddFormController extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+@Controller
+public class CommentAddFormController {
 
-  // MemberDao memberDao;
-  StudyDao studyDao;
   FreeBoardDao freeBoardDao;
   CommentDao commentDao;
 
-  @Override
-  public void init(ServletConfig config) throws ServletException {
-    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
-    // memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
-    studyDao = (StudyDao) 웹애플리케이션공용저장소.getAttribute("studyDao");
-    freeBoardDao = (FreeBoardDao) 웹애플리케이션공용저장소.getAttribute("freeBoardDao");
-    commentDao = (CommentDao) 웹애플리케이션공용저장소.getAttribute("commentDao");
-  }
+  @GetMapping("/freeboard/comment/form")
+  public ModelAndView form(int freeboardno, int studyno) throws Exception {
+    // Study study = studyDao.findByNo(studyNo);
+    FreeBoard freeBoard = freeBoardDao.findByNo(freeboardno, studyno);
 
-  @Override
-  protected void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+    ModelAndView mv = new ModelAndView();
 
-    try {
-      // int no = Integer.parseInt(request.getParameter("perNo"));
-      // Member member = memberDao.findByNo(no);
+    mv.addObject("studyno", freeBoard.getStudyNo());
+    mv.addObject("freeboardno", freeBoard.getFreeBoardNo());
+    mv.setViewName("redirect:detail?studyno=" + freeBoard.getStudyNo() + "&freeboardno="
+        + freeBoard.getFreeBoardNo());
 
-      int studyNo = Integer.parseInt(request.getParameter("studyno"));
-      Study study = studyDao.findByNo(studyNo);
-
-      int freeBoardNo = Integer.parseInt(request.getParameter("freeboardno"));
-      FreeBoard freeBoard = freeBoardDao.findByNo(freeBoardNo, studyNo);
-
-      // request.setAttribute("member", member);
-      request.setAttribute("study", study);
-      request.setAttribute("freeBoard", freeBoard);
-      response.sendRedirect("../detail?studyno=" + freeBoard.getStudyNo() + "&freeboardno=" + freeBoard.getFreeBoardNo());
-
-    } catch (Exception e) {
-      e.printStackTrace();
-      request.setAttribute("error", e);
-      request.getRequestDispatcher("/Error.jsp").forward(request, response);
-    }
+    return mv;
   }
 }
