dd7ab177eb9faac4cdceb34b2086c8f68da6b0a2
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-11-13 18:46:44 +0900

    1113 push StudyBookMarkList 스프링 적용 및 테스트 완료

--- app-server/src/main/java/com/ogong/pms/web/study/bookMark/StudyBookMarkListController.java
+++ app-server/src/main/java/com/ogong/pms/web/study/bookMark/StudyBookMarkListController.java
@@ -1,53 +1,36 @@
 package com.ogong.pms.web.study.bookMark;
 
-import java.io.IOException;
 import java.util.Collection;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 import com.ogong.pms.dao.MemberDao;
 import com.ogong.pms.dao.StudyDao;
 import com.ogong.pms.domain.Member;
 import com.ogong.pms.domain.Study;
 
-@WebServlet("/bookmark/list")
-public class StudyBookMarkListController extends HttpServlet {
-  private static final long serialVersionUID = 1L;
+@Controller
+public class StudyBookMarkListController {
 
+  @Autowired
   MemberDao memberDao;
+  @Autowired
   StudyDao studyDao;
 
-  @Override
-  public void init(ServletConfig config) throws ServletException {
-    ServletContext 웹애플리케이션공용저장소 = config.getServletContext();
-    memberDao = (MemberDao) 웹애플리케이션공용저장소.getAttribute("memberDao");
-    studyDao = (StudyDao) 웹애플리케이션공용저장소.getAttribute("studyDao");
-  }
+  @GetMapping("/bookmark/list")
+  public ModelAndView list(HttpSession session) throws Exception {
+    Collection<Study> studyList =
+        studyDao.findByMyBookmark(((Member) session.getAttribute("loginUser")).getPerNo());
 
-  @Override
-  public void service(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+    ModelAndView mv = new ModelAndView();
 
-    try {
-      Member loginUser = (Member) request.getSession().getAttribute("loginUser");
-      Member member = memberDao.findByNo(loginUser.getPerNo());
+    mv.addObject("studyList", studyList);
+    mv.addObject("pageTitle", "북마크 목록");
+    mv.addObject("contentUrl", "study/bookMark/StudyBookMarkList.jsp");
+    mv.setViewName("template1");
 
-      Collection<Study> studyList = studyDao.findByMyBookmark(member.getPerNo());
-
-      // request.setAttribute("member", member);
-      request.setAttribute("studyList", studyList);
-      request.setAttribute("pageTitle", "북마크 목록");
-      request.setAttribute("contentUrl", "/study/bookMark/StudyBookMarkList.jsp");
-      request.getRequestDispatcher("/template1.jsp").forward(request, response);
-
-    } catch (Exception e) {
-      e.printStackTrace();
-      request.setAttribute("error", e);
-      request.getRequestDispatcher("/Error.jsp").forward(request, response);
-    }
+    return mv;
   }
 }
