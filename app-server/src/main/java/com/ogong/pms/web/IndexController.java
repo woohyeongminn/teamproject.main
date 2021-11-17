72e6a84e02c435c4b7ba73038a79dd53e3933a94
jo-sol <zoixssm@gmail.com>
2021-11-17 20:30:58 +0900

    1117 push 메인 링크 걸기까지 완료

--- app-server/src/main/java/com/ogong/pms/web/IndexController.java
+++ app-server/src/main/java/com/ogong/pms/web/IndexController.java
@@ -1,11 +1,18 @@
 package com.ogong.pms.web;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import com.ogong.pms.dao.StudyDao;
+import com.ogong.pms.domain.Study;
 
 @Controller
 public class IndexController {
+
+  @Autowired StudyDao studyDao;
 
   @GetMapping("/index")
   public ModelAndView index() {
@@ -16,8 +23,26 @@ public class IndexController {
   }
 
   @GetMapping("/index2")
-  public ModelAndView index2() {
+  public ModelAndView index2() throws Exception {
+
+    List<Study> findstudyList = studyDao.findAll();
+
+    if (findstudyList == null) {
+      throw new Exception("스터디 정보가 없습니다.");
+    }
+
+    Study study = new Study();
+    List<Study> studyList = new ArrayList<>();
+
+    for (int i = 0; i < findstudyList.size(); i++) {
+      if (findstudyList.get(i).getReport() != 2 && studyList.size() < 6) {
+        study = findstudyList.get(i);
+        studyList.add(study);
+      }
+    }
+
     ModelAndView mv = new ModelAndView();
+    mv.addObject("studyList", studyList);
     mv.addObject("pageTitle", "오늘의 공부");
     mv.setViewName("index2");
     return mv;
