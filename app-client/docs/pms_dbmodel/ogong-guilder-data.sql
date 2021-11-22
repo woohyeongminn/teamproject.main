bddbc6bccf14da90cbac8e603d83d66efd81c6c1
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-11-23 00:57:14 +0900

    1123 push study 테스트 값 날짜 재정렬

--- app-client/docs/pms_dbmodel/ogong-guilder-data.sql
+++ app-client/docs/pms_dbmodel/ogong-guilder-data.sql
@@ -1,12 +1,12 @@
-/* 1 = 승인대기 / 2 = 승인 / 3 = 거절 */
+/* 1 = 승인대기 / 2 = 승인 */
 
--- 스터디구성원 test 값
-
-insert into study_guilder(member_no,study_no,created_dt,status) 
-values(1,2,'2021-3-25',2);
+-- 스터디 구성원 값
 
 insert into study_guilder(member_no,study_no,created_dt,status) 
-values(1,3,'2021-3-25',2);
+values(1,1,'2021-3-25',2);
+
+insert into study_guilder(member_no,study_no,created_dt,status) 
+values(2,1,'2021-3-25',2);
 
 insert into study_guilder(member_no,study_no,created_dt,status) 
 values(1,5,'2021-07-30',1);
@@ -15,10 +15,10 @@ insert into study_guilder(member_no,study_no,created_dt,status)
 values(1,7,'2021-10-25',2);
 
 insert into study_guilder(member_no,study_no,created_dt,status) 
-values(2,8,'2021-10-25',2);
+values(1,8,'2021-10-25',2);
 
 insert into study_guilder(member_no,study_no,created_dt,status) 
-values(3,1,'2021-08-25',2);
+values(1,1,'2021-08-25',2);
 
 insert into study_guilder(member_no,study_no,created_dt,status) 
 values(3,4,'2021-10-25',2);
@@ -46,6 +46,3 @@ values(7,2,'2021-06-05',1);
 
 insert into study_guilder(member_no,study_no,created_dt,status) 
 values(7,6,'2021-10-25',2);
-
--- 포인트 테스트값
-insert into point(member_no, study_no, point) values(1, 1, 100);
