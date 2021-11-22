2c916bec1712da009ab2dc71ffc3d2eae38e8c05
Kimeunchaee <eun00218@naver.com>
2021-11-23 03:46:28 +0900

    1123 push ceoMember 테스트값 등록일 컬럼 추가해서 수정

--- app-client/docs/pms_dbmodel/ogong-ceomember-data.sql
+++ app-client/docs/pms_dbmodel/ogong-ceomember-data.sql
@@ -11,40 +11,40 @@ insert into member(member_no, name, nickname, email, password, tel, photo, creat
 values(10, '우형민', '민민', 'cafe3@naver.com', password('c3'), '010-1024-9845',
 'ceoProfile_color','2020-03-01', 2);
 
-insert into member(member_no, name, nickname, email, password, tel, photo, status) 
+insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(11, '조솔', '솔솔', 'cafe4@naver.com', password('c4'), '010-2245-7522', 
 'ceoProfile_color','2020-03-07', 2);
 
-insert into member(member_no, name, nickname, email, password, tel, photo, status) 
+insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(12, '반계령', '령령', 'cafe5@naver.com', password('c5'), '010-7158-8691', 
 'ceoProfile','2020-04-28', 2);
 
-insert into member(member_no, name, nickname, email, password, tel, photo, status) 
+insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(13, '이승호', '호호사장', 'cafe6@naver.com', password('c6'), '010-7122-7584', 
 'ceoProfile_color','2020-05-11', 2);
 
-insert into member(member_no, name, nickname, email, password, tel, photo, status) 
+insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(14, '박성빈', '빈이네', 'cafe7@naver.com', password('c7'), '010-7869-2013', 
 'ceoProfile','2020-06-12', 2);
 
-insert into member(member_no, name, nickname, email, password, tel, photo, status) 
+insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(15, '한재영', '영재쓰', 'cafe8@naver.com', password('c8'), '010-8752-5274', 
 'ceoProfile_color','2020-07-27', 2);
 
-insert into member(member_no, name, nickname, email, password, tel, photo, status) 
+insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(16, '차은우', '얼천', 'cafe9@naver.com', password('c9'), '010-9415-2257', 
 'ceoProfile','2020-12-12', 2);
 
 
-insert into member(member_no, name, nickname, email, password, tel, photo, status) 
+insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(25, '박태은', '오호라', 'cafe10@naver.com', password('c10'), '010-5231-7851', 
 'ceoProfile','2021-03-18', 2);
 
-insert into member(member_no, name, nickname, email, password, tel, photo, status) 
+insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(26, '홍수정', '이런저런', 'cafe11@naver.com', password('c11'), '010-4675-0215', 
 'ceoProfile','2021-08-22', 2);
 
-insert into member(member_no, name, nickname, email, password, tel, photo, status) 
+insert into member(member_no, name, nickname, email, password, tel, photo, created_dt, status) 
 values(27, '구시영', '구구구', 'cafe12@naver.com', password('c12'), '010-3648-3485', 
 'ceoProfile_color','2021-11-11', 2);
 
