c0bef76be3116000d013a8898286a894db9931a4
Gyeryeong Ban <gyeryeong.ban@outlook.com>
2021-11-23 04:12:32 +0900

    1123 push study 테스트 값 대면 상태 및 지역 수정

--- app-client/docs/pms_dbmodel/ogong-study-data.sql
+++ app-client/docs/pms_dbmodel/ogong-study-data.sql
@@ -18,19 +18,19 @@ insert into study_face_status(face_no, name) values(3, '대면/비대면');
 -- 스터디 값
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status) 
-values(1, '백엔드 취업 스터디 모집', 4, 6, 3, '내년 상반기 취업 및 재취업을 목표로 서로 정보공유를 하며 꾸준히 같이 달리실 분 모집합니다! 먼저 CS, Java, Spring, JPA 위주로 공부합니다. 백엔드 개발자라 하더라도 실제 면접에서 Docker 및 k8s와 같은 인프라 지식도 물어볼 수 있기 때문에 해당 내용도 어느정도 공부할 예정입니다. 저도 직장인이기 때문에 재직자도 환영입니다.', '2021-11-1', 1, 1);
+values(1, '백엔드 취업 스터디 모집', 4, 6, 2, '내년 상반기 취업 및 재취업을 목표로 서로 정보공유를 하며 꾸준히 같이 달리실 분 모집합니다! 먼저 CS, Java, Spring, JPA 위주로 공부합니다. 백엔드 개발자라 하더라도 실제 면접에서 Docker 및 k8s와 같은 인프라 지식도 물어볼 수 있기 때문에 해당 내용도 어느정도 공부할 예정입니다. 저도 직장인이기 때문에 재직자도 환영입니다.', '2021-11-1', 1, 1);
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status, area) 
-values(2, 'Salesforce 개발자 자격증', 2, 4, 2, '함께 동영상(영어로 제작된 교육)을 보면서 하나씩 공부 예정. 세일즈포스 홈페이지에 많이 자료가 있어서 함께 찾아가면서 공부할 분을 찾아요.', '2021-11-2', 2, 1, '서울시');
+values(2, 'Salesforce 개발자 자격증', 2, 4, 1, '함께 동영상(영어로 제작된 교육)을 보면서 하나씩 공부 예정. 세일즈포스 홈페이지에 많이 자료가 있어서 함께 찾아가면서 공부할 분을 찾아요.', '2021-11-2', 2, 1, '서울시');
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status) 
-values(3, '영어 공부 하실 분', 1, 2, 1, '진지하게 영어 공부하실 분만 신청 바랍니다', '2021-11-3', 3, 1);
+values(3, '영어 공부 하실 분', 1, 2, 2, '진지하게 영어 공부하실 분만 신청 바랍니다', '2021-11-3', 3, 1);
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status, area) 
-values(4, '코딩테스트 스터디 모집합니다', 4, 8, 1, '온라인 모임으로 백준 기준 골드 혹은 프로그래머스 2,3단계 - 수준 3~4 문제 위주로 각자 풀이 후 모임에서 자신의 풀이 설명', '2021-11-4', 4, 2, '강남구');
+values(4, '코딩테스트 스터디 모집합니다', 4, 8, 3, '온라인 모임으로 백준 기준 골드 혹은 프로그래머스 2,3단계 - 수준 3~4 문제 위주로 각자 풀이 후 모임에서 자신의 풀이 설명', '2021-11-4', 4, 2, '강남구');
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status) 
@@ -38,19 +38,19 @@ values(5, '기타 치실 분', 5, 10, 2, '기타 잘 못 쳐도 환영합니다 
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status, area) 
-values(6, '주말 등산 스터디', 6, 12, 3, '등산은 장비빨', '2021-11-6', 6, 2, '안양시');
+values(6, '주말 등산 스터디', 6, 12, 1, '등산은 장비빨', '2021-11-6', 6, 2, '안양시');
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status) 
-values(7, '프랑스어 공부 하실 분', 1, 14, 1, '열심히 참여하실 분 환영 합니다', '2021-11-7', 7, 1);
+values(7, '프랑스어 공부 하실 분', 1, 14, 2, '열심히 참여하실 분 환영 합니다', '2021-11-7', 7, 1);
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status, area) 
-values(8, '컴퓨터 활용능력 1급 스터디', 2, 16, 2, '인강을 통해 학습, 메신저와 대면 스터디로 질답 공유', '2021-11-8', 17, 2, '시흥시');
+values(8, '컴퓨터 활용능력 1급 스터디', 2, 16, 3, '인강을 통해 학습, 메신저와 대면 스터디로 질답 공유', '2021-11-8', 17, 2, '시흥시');
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status) 
-values(9, 'CS / Java / Spring 모의 면접 스터디 팀원 구합니다 !', 3, 18, 3, 'CS, Java, Spring 모의 면접 진행할 팀원 모집', '2021-11-9', 18, 1);
+values(9, 'CS / Java / Spring 모의 면접 스터디 팀원 구합니다 !', 3, 18, 2, 'CS, Java, Spring 모의 면접 진행할 팀원 모집', '2021-11-9', 18, 1);
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status, area) 
@@ -66,19 +66,19 @@ values(12, '식물 키우기 스터디 모집', 6, 24, 3, '식물 자주 죽이�
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status) 
-values(13, '독일어 공부 하실 분', 1, 26, 1, '성실하게 참여하실 분 아니면 신청하지도 마세요', '2021-11-13', 22, 1);
+values(13, '독일어 공부 하실 분', 1, 26, 2, '성실하게 참여하실 분 아니면 신청하지도 마세요', '2021-11-13', 22, 1);
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status, area) 
-values(14, '방학기념 컴퓨터 초보를 위한 스터디', 2, 28, 2, '방학 동안 컴퓨터활용능력을 기르고 자격증까지 취득할 수 있는 스터디를 진행하고자 합니다. 원하시는 다른 주제가 있다면 스터디원분들과 같이 정하고 싶습니다!', '2021-11-14', 23, 2, '제주시');
+values(14, '방학기념 컴퓨터 초보를 위한 스터디', 2, 28, 1, '방학 동안 컴퓨터활용능력을 기르고 자격증까지 취득할 수 있는 스터디를 진행하고자 합니다. 원하시는 다른 주제가 있다면 스터디원분들과 같이 정하고 싶습니다!', '2021-11-14', 23, 2, '제주시');
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status) 
-values(15, '취업용 포트폴리오 스터디 모집', 3, 30, 3, '취업을 위한 웹사이트 제작 및 배포가 목표 입니다. 혼자서 포트폴리오 제작 하시는데 어려움을 겪고 계신분들 환영 합니다.', '2021-11-15', 24, 1);
+values(15, '취업용 포트폴리오 스터디 모집', 3, 30, 2, '취업을 위한 웹사이트 제작 및 배포가 목표 입니다. 혼자서 포트폴리오 제작 하시는데 어려움을 겪고 계신분들 환영 합니다.', '2021-11-15', 24, 1);
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status, area) 
-values(16, '스프링인액션 제5판', 4, 5, 1, '도서 "스프링인액션(5판)" 제대로 파보기 함께 마음맞는 분들과 스터디를 진행하며 실력향상을 도모하고 싶습니다!', '2021-11-16', 1, 2, '강원도');
+values(16, '스프링인액션 제5판', 4, 5, 3, '도서 "스프링인액션(5판)" 제대로 파보기 함께 마음맞는 분들과 스터디를 진행하며 실력향상을 도모하고 싶습니다!', '2021-11-16', 1, 2, '강원도');
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status) 
@@ -86,7 +86,7 @@ values(17, '스우파 헤이마마 춤 마스터', 5, 15, 2, '스우파 애청�
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status, area) 
-values(18, '클래식 독서 모임', 6, 25, 3, '클래식 들으며 같이 책 읽어요', '2021-11-18', 1, 2, '전라도');
+values(18, '클래식 독서 모임', 6, 25, 1, '클래식 들으며 같이 책 읽어요', '2021-11-18', 1, 2, '전라도');
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status) 
@@ -94,7 +94,7 @@ values(19, '일본어 공부 하실 분', 1, 3, 2, '수강 인증 날짜 잘 지
 
 insert into study(
 study_no, name, subject_no, no_people, face_no, introduction, created_dt, member_no, status, area) 
-values(20, '웹디자인기능사 실기시험 완벽 가이드 완강까지 함께 하실 분~!', 2, 13, 3, '최소 주2회 공부한 내용 올리고 공유하기 강의를 수강하며 공부한 내용을 정리해서 사진 인증', '2021-11-20', 1, 2, '부천시');
+values(20, '웹 디자인 기능사 실기 시험 완벽 가이드 완강까지 함께 하실 분~!', 2, 13, 3, '최소 주2회 공부한 내용 올리고 공유하기 강의를 수강하며 공부한 내용을 정리해서 사진 인증', '2021-11-20', 1, 2, '부천시');
 
 -------------------------------------------------------------------------------------------------
 
